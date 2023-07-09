02f76d0dde02efc32bdcc7bb6686b0b4a3fb5530
Drink7 <johnnyliu505@gmail.com>
2023-07-09 23:18:33 +0800

    Time: 8 ms (50.91%), Space: 10.4 MB (37.64%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        /*
         // Hash Table
         // TC: O(n)
         // SC: O(n)
@@ -17,6 +18,20 @@ public:
         }
 
         ans.assign(intersecSet.begin(), intersecSet.end());
+        return ans;
+        */
+
+        // Hash Table + optimization
+        // TC: O(n)
+        // SC: O(n)
+        unordered_set<int> nums1Set(nums1.begin(), nums1.end());
+        vector<int> ans;
+        for (auto const& num : nums2) {
+            if (nums1Set.count(num)) {
+                ans.push_back(num);
+                nums1Set.erase(num);
+            }
+        }
         return ans;
     }
 };
