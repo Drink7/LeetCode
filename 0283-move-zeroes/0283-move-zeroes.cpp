c28abc7eaf584f79a0a357b64471a24c495266d7
Drink7 <johnnyliu505@gmail.com>
2023-07-09 21:39:38 +0800

    Time: 34 ms (18.73%), Space: 19.1 MB (96.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // insertion sort
+        /*
+        // insertion sort concept
         // TC: O(n^2)
         // SC: O(1)
         int zeroStart = 0;
@@ -16,6 +17,22 @@ public:
                 nums[j + 1] = val;
                 zeroStart++;
             }
+        }
+        */
+        // two pointer
+        // TC: O(n)
+        // SC: O(1)
+        int j = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+
+        // put zero at the end
+        for (int i = j; i < n; i++) {
+            nums[i] = 0;
         }
     }
 };
