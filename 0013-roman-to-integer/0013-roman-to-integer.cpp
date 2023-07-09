e1cb1fc0e23a1fdb42f13f4a2f9e01dea1ce2c88
Drink7 <johnnyliu505@gmail.com>
2023-07-10 00:44:19 +0800

    Time: 22 ms (28.35%), Space: 7.7 MB (54.81%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        // Traverse from right to left
+        // TC: O(n)
+        // SC: O(n)
+        int val = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n &&
+                ((s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X')) ||
+                (s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C')) ||
+                (s[i] == 'C' && (s[i + 1] == 'D' || s[i + 1] == 'M')))) {
+                val += dict[s[i + 1]] - dict[s[i]];
+                i++;
+            } else {
+                val += dict[s[i]];
+            }
+        }
+        return val;
+    }
+private:
+    unordered_map<char, int> dict{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+};
