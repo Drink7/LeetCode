692906eb57c57f5da4daeb240ca00ee307f6dcad
Drink7 <johnnyliu505@gmail.com>
2023-07-09 15:37:03 +0800

    Time: 17 ms (60.08%), Space: 10.7 MB (44.59%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> dict;
+        for (int i = 0; i < nums.size(); i++) {
+            int remain = target - nums[i];
+            if (dict.count(remain)) {
+                return {i, dict[remain]};
+            }
+            dict[nums[i]] = i;
+        }
+
+        // impossible
+        return {-1, -1};
+    }
+};
