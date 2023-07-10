cff0697c82b6a0edf7d2fb91bbcca720a4efbf58
Drink7 <johnnyliu505@gmail.com>
2023-07-10 22:03:07 +0800

    Time: 0 ms (100%), Space: 6.5 MB (39.3%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,4 +1,5 @@
 class Solution {
+/*
 public:
     vector<int> getRow(int rowIndex) {
         // DP top down + memorization
@@ -16,4 +17,20 @@ public:
     }
 private:
     unordered_map<int, vector<int>> dict = {{0, {1}}, {1, {1, 1}}};
+*/
+public:
+    vector<int> getRow(int rowIndex) {
+        // DP bottom up
+        // TC: O(n^2)
+        // SC: O(n)
+        // Iteratively update from the end to the beginning
+        vector<int> result(rowIndex + 1, 0);
+        result[0] = 1;
+        for (int i = 1; i < rowIndex + 1; i++) {
+            for (int j = i; j >= 1; j--) {
+                result[j] += result[j - 1];
+            }
+        }
+        return result;
+    }
 };
