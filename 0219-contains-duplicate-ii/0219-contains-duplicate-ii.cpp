bb0003a69574cba309531c252db40e0e370bbafb
Drink7 <johnnyliu505@gmail.com>
2023-07-11 21:41:01 +0800

    Time: 202 ms (49.7%), Space: 72.3 MB (96.34%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        /*
         // hash table and two loop traverse
         // TC: O(n^2)
         // SC: O(n)
@@ -20,6 +21,23 @@ public:
                         return true;
                     }
                 }
+            }
+        }
+        return false;
+        */
+        // Sliding window
+        // TC: O(n)
+        // SC: O(min(n, k))
+        unordered_set<int> s;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i > k) {
+                s.erase(nums[i - k - 1]);
+            }
+            if (s.count(nums[i])) {
+                return true;
+            } else {
+                s.insert(nums[i]);
             }
         }
         return false;
