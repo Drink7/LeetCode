aaa3b15d2a3774addb99f482aab1d6e972e29575
Drink7 <johnnyliu505@gmail.com>
2023-07-11 23:27:14 +0800

    Time: 35 ms (60.42%), Space: 25.4 MB (40.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        /*
         // Copy a larger space (numsCopy) which width is nums + (nums.size() - k)
         // and assign the nums from the array numsCopy, starting by nums.size() - (numsCopy).end();
         // TC: O(n)
         // SC: O(n)
         int n = nums.size();
-        int actualK = k % n;
+        int rotateK = k % n;
         vector<int> numsCopy(nums.begin(), nums.end());
         // copy nums.size() - k element to numsCopy
-        for (int i = 0; i < n - actualK; i++) {
+        for (int i = 0; i < n - rotateK; i++) {
             numsCopy.push_back(nums[i]);
         }
 
-        // assign the nums
-        nums.assign(numsCopy.begin() + n - actualK, numsCopy.end());
+        // Assign the nums
+        nums.assign(numsCopy.begin() + n - rotateK, numsCopy.end());
+        */
+        // Assign the nums from the array numsCopy
+        // TC: O(n)
+        // SC: O(n)
+        int n = nums.size();
+        vector<int> numsCopy = nums;
+        for (int i = 0; i < n; i++) {
+            nums[(i + k) % n] = numsCopy[i];
+        }
     }
 };
