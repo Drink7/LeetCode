e54bc38b3cc59a04e62e04960eebcb0ed669405e
Drink7 <johnnyliu505@gmail.com>
2023-07-14 01:02:56 +0800

    Time: 0 ms (100%), Space: 7.4 MB (12.73%) - LeetHub

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        // Prefix sum to get the minimum value
+        // TC: O(n)
+        // SC: O(n)
+        int n = nums.size();
+        vector<int> prefixSum(nums.begin(), nums.end());
+        int minVal = prefixSum[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] += prefixSum[i - 1];
+            minVal = min(minVal, prefixSum[i]);
+        }
+        return (minVal <= 0) ? 1 - minVal : 1;
+    }
+};
