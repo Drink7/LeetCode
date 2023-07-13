4150014e56dc9812e228e7b2849725eb57402c3e
Drink7 <johnnyliu505@gmail.com>
2023-07-14 01:06:26 +0800

    Time: 0 ms (100%), Space: 7.3 MB (97.32%) - LeetHub

--- 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
+        /*
         // Prefix sum to get the minimum value
         // TC: O(n)
         // SC: O(n)
@@ -12,5 +13,18 @@ public:
             minVal = min(minVal, prefixSum[i]);
         }
         return (minVal <= 0) ? 1 - minVal : 1;
+        */
+        // Prefix sum to get the minimum value
+        // Space optimization
+        // TC: O(n)
+        // SC: O(n)
+        int n = nums.size();
+        int prefixSum = 0;
+        int minVal = 0;
+        for (int i = 0; i < n; i++) {
+            prefixSum += nums[i];
+            minVal = min(minVal, prefixSum);
+        }
+        return 1 - minVal;
     }
 };
