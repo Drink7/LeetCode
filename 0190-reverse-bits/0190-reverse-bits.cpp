810e2fc3bfbc88742d711699d53ab76a74b4eb2e
Drink7 <johnnyliu505@gmail.com>
2023-07-27 21:00:51 +0800

    Time: 0 ms (100%), Space: 5.9 MB (60.87%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
+        /*
         // shift left
         // TC: O(1)
         // SC: O(1)
@@ -10,6 +11,19 @@ public:
             reversed = (reversed | (n & 1));
             n >>= 1;
         }
+        return reversed;
+        */
+
+        // shift left
+        // TC: O(1)
+        // SC: O(1)
+        uint32_t reversed = 0;
+        for (int i = 1; i < 32; i++) {
+            reversed = (reversed | (n & 1));
+            reversed = reversed << 1;
+            n = n >> 1;
+        }
+        reversed = (reversed | (n & 1));
         return reversed;
     }
 };
