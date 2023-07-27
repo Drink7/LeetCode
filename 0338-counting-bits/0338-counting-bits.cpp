b90dce90aa4768cc8cb50d128d4bd4be9a242ba6
Drink7 <johnnyliu505@gmail.com>
2023-07-27 19:06:59 +0800

    Time: 8 ms (48.94%), Space: 8.5 MB (31.7%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+        // 0 1 1 2 1 2 2 3 1 2 2  3  2  3  3  4  1
+        // >> 1: right shift 1 is to divide by 2
+        // dynamic programming
+        // TC: O(nlogn)
+        // SC: O(n)
+        vector<int> result;
+        for (int i = 0; i <= n; i++) {
+            int sum = 0;
+            int num = i;
+            while (num != 0) {
+                sum += num % 2;
+                num = num / 2;
+            }
+            result.push_back(sum);
+        }
+        return result;
+    }
+};
