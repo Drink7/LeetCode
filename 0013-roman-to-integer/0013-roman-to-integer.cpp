b0b5bfa1ef366c111dead5b32a8c3f72a5ebf6e3
Drink7 <johnnyliu505@gmail.com>
2023-07-30 14:51:10 +0800

    Time: 11 ms (75.43%), Space: 7.8 MB (59.92%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        /*
-        // Traverse
-        // TC: O(n)
-        // SC: O(n)
-        int val = 0;
+        // Notice the 4, 9 cases
         int n = s.size();
+        int result = 0;
         for (int i = 0; i < n; i++) {
-            if (i + 1 < n &&
-                ((s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X')) ||
-                (s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C')) ||
-                (s[i] == 'C' && (s[i + 1] == 'D' || s[i + 1] == 'M')))) {
-                val += dict[s[i + 1]] - dict[s[i]];
-                i++;
+            if (i + 1 < n && dict[s[i + 1]] > dict[s[i]]) {
+                result -= dict[s[i]];
             } else {
-                val += dict[s[i]];
+                result += dict[s[i]];
             }
         }
-        return val;
-        */
-        // Roman numerals are usually written largest to smallest from left to right
-        // XII (7), XXVII (27), III (3)...
-        // If a small value is placed before a bigger value then it's a combine number
-        // IV (4), IX (9), XIV (14)
-        // IV = -1 + 5
-        // TC: O(n)
-        // SC: O(n)
-        int val = 0;
-        int n = s.size();
-        for (int i = 0; i < n; i++) {
-            if (i < n - 1 && dict[s[i]] < dict[s[i + 1]]) {
-                val -= dict[s[i]];
-            } else {
-                val += dict[s[i]];
-            }
-        }
-        return val;
+        return result;
     }
 private:
-    unordered_map<char, int> dict{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+    unordered_map<char, int> dict = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 };
