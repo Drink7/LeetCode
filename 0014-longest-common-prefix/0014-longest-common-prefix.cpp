d1fae3055712751d28d3791deedef696164901bb
Drink7 <johnnyliu505@gmail.com>
2023-07-30 23:08:27 +0800

    Time: 0 ms (100%), Space: 9.2 MB (61.59%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        // Choose the first string as token
+        // start from the second string and get the common prefix
+        // until the last string
+        // TC: O(m * n), m is average string length, n is string list number
+        // SC: O(1)
+        string base = strs[0];
+        int n = strs.size();
+        for (int i = 1; i < n; i++) {
+            while (strs[i].find(base) == string::npos || strs[i].find(base) != 0) {
+                base = base.substr(0, base.size() - 1);
+            }
+        }
+        return base;
+    }
+};
