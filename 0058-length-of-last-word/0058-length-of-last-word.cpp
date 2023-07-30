a09544e5e92c55bcc58f6b71298e73d01d0ad4d7
Drink7 <johnnyliu505@gmail.com>
2023-07-30 16:20:57 +0800

    Time: 4 ms (39.05%), Space: 6.9 MB (6.2%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        // Word split and find the n - 1 part
+        // return its length
+        // TC: O(n)
+        // SC: O(1)
+        stringstream ss(s);
+        string token;
+
+        while (ss >> token) {
+        }
+
+        return token.size();
+    }
+};
