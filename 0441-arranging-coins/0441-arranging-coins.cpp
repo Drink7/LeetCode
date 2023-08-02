761f5b9e4d7829400839069c9077f03001f4c7ce
Drink7 <johnnyliu505@gmail.com>
2023-08-03 00:39:46 +0800

    Time: 0 ms (100%), Space: 5.9 MB (87.63%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        // 1 + 2 + 3 + ... + m <= n
+        // TC: O(nlogn)
+        // SC: O(1)
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long sum = (1 + mid) * mid / 2;
+
+            if (sum <= n) {
+                if ((1 + 1 + mid) * (1 + mid) / 2 > n) {
+                    return mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
