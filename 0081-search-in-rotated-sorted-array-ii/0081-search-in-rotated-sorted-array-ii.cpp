ec458c4d0b7105619467b073268c2c59095f545c
Drink7 <johnnyliu505@gmail.com>
2023-08-02 21:46:59 +0800

    Time: 3 ms (92.85%), Space: 13.9 MB (88.08%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        // Similar to LC. 33
+        // Handle duplicate
+        // TC: O(logn)
+        // SC: O(1)
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[left] == nums[mid] && nums[right] == nums[mid]) {
+                // The condition arr[mid] == arr[l] == arr[r] can be checked before the original logic
+                // if so then move left and right both towards the middle by 1 and repeat.
+                left++;
+                right--;
+            } else if (nums[mid] > nums[right]) {
+                // Array must be rotated
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // Array might be rotated
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
