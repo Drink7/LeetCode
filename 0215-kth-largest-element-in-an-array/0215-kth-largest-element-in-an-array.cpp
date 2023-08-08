f37a2c747a6f44c5ae8f686be7c4d17c32337079
Drink7 <johnnyliu505@gmail.com>
2023-08-08 23:45:00 +0800

    Time: 143 ms (34.94%), Space: 45.5 MB (85.62%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,19 +1,52 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        /*
         // MaxHeap Priority Queue
         // TC: O(nlogn + k)
         // SC: O(n)
-        priority_queue<int> maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
         for (auto const& num : nums) {
-            maxHeap.push(num);
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
         }
 
-        int res = 0;
-        for (int i = 0; i < k; i++) {
-            res = maxHeap.top();
-            maxHeap.pop();
+        return minHeap.top();
+        */
+        // Quick Select
+        // find k largest, quick select n - k large
+        // TC: O(n)
+        // SC: O(1)
+        return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
+    }
+
+    int quickSelect(vector<int>& nums, int left, int right, int k) {
+        if (left == right) {
+            return nums[left];
         }
-        return res;
+
+        int pivot = partition(nums, left, right);
+        if (pivot == k) {
+            return nums[k];
+        } else if (pivot > k) {
+            return quickSelect(nums, left, pivot - 1, k);
+        } else {
+            return quickSelect(nums, pivot + 1, right, k);
+        }
+    }
+
+    int partition(vector<int>& nums, int left, int right) {
+        int i = left;
+        int pivot = nums[right];
+        for (int j = left; j < right; j++) {
+            if (nums[j] <= pivot) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+        swap(nums[i], nums[right]);
+        return i;
     }
 };
