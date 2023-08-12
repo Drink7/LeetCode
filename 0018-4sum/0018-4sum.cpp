01e4c6fa662f5d5ee7bf3d4c4953f6ce1f03a1e2
Drink7 <johnnyliu505@gmail.com>
2023-08-12 16:56:35 +0800

    Time: 4 ms (99.77%), Space: 13.8 MB (33.05%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        // sort first
-        // Same concept in 3Sum but would be O(n^3) here, TLE?
+        /*
+        // Sort first
+        // Same concept in 3Sum but would be O(n^3) here
         // TC: O(n^3)
         // SC: O(1)
         sort(nums.begin(), nums.end());
@@ -43,6 +44,60 @@ public:
                         left++;
                     }
                 }
+            }
+        }
+        return result;
+        */
+
+        // More general, N-Sum (Base on two sum)
+        sort(nums.begin(), nums.end());
+        return NSum(nums, 0, nums.size() - 1, target, 4);
+    }
+    vector<vector<int>> NSum(vector<int>& nums, int left, int right, long long int target, int n) {
+        vector<vector<int>> results;
+        if (right - left + 1 < n || (long long int) nums[left] * n > target || (long long int)nums[right] * n < target) {
+            return results;
+        }
+        if (n == 2) {
+            return twoSum(nums, left, right, target);
+        } else {
+            for (int i = left; i < right; i++) {
+                if (i > left && nums[i - 1] == nums[i]) {
+                    continue;
+                }
+                int element = nums[i];
+                vector<vector<int>> resultFromNSum = NSum(nums, i + 1, right, target - element, n - 1);
+                for (vector<int> v : resultFromNSum) {
+                    v.push_back(element);
+                    results.push_back(v);
+                }
+            }
+        }
+        return results;
+    }
+    
+    vector<vector<int>> twoSum(vector<int>& nums, int left, int right, long long int target) {
+        vector<vector<int>> result;
+        
+        while (left < right) {
+            long long int sum = nums[left] + nums[right];
+            if (sum == target) {
+                result.push_back({nums[left], nums[right]});
+                left++;
+                right--;
+
+                // skip repeat
+                while (left < right && nums[left - 1] == nums[left]) {
+                    left++;
+                }
+
+                while (left < right && nums[right + 1] == nums[right]) {
+                    right--;
+                }
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
         return result;
