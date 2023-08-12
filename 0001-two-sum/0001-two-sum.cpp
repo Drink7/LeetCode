69e375a0eaff15520bbc4f9ee0a892bb293f245c
Drink7 <johnnyliu505@gmail.com>
2023-08-12 13:40:05 +0800

    Time: 7 ms (95.36%), Space: 10.9 MB (31.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        /*
         // Hash table (unorder_map)
         // TC: O(n)
         // SC: O(n)
@@ -15,5 +16,31 @@ public:
             }
         } 
         return {-1, -1};
+        */
+        // Two pointer with sorted
+        // TC: O(nlogn)
+        // SC: O(n)
+        vector<pair<int, int>> numsArr;
+        for (int i = 0; i < nums.size(); i++) {
+            numsArr.push_back({nums[i], i});
+        }
+
+        sort(numsArr.begin(), numsArr.end(), [](pair<int, int>& a, pair<int, int>& b) {
+            return a.first < b.first;
+        });
+
+        int left = 0;
+        int right = numsArr.size() - 1;
+        while (left < right) {
+            int sum = numsArr[left].first + numsArr[right].first;
+            if (sum == target) {
+                return {numsArr[left].second, numsArr[right].second};
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return {};
     }
 };
