b124d61379e30a472e6ec3b67ce01f6d46a7f51f
Drink7 <johnnyliu505@gmail.com>
2023-08-12 13:19:41 +0800

    Time: 7 ms (95.36%), Space: 10.7 MB (43.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Hash table (unorder_map)
         // TC: O(n)
         // SC: O(n)
+        // store val, id
         unordered_map<int, int> dict;
         for (int i = 0; i < nums.size(); i++) {
             int remain = target - nums[i];
             if (dict.count(remain)) {
-                return {i, dict[remain]};
+                return {dict[remain], i};
+            } else {
+                dict[nums[i]] = i;
             }
-            dict[nums[i]] = i;
-        }
-
-        // impossible
+        } 
         return {-1, -1};
     }
 };
