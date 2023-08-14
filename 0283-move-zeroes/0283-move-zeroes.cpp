1e716833d1c770377e715dd18c3c19c07792a2c0
Drink7 <johnnyliu505@gmail.com>
2023-08-14 23:59:46 +0800

    Time: 17 ms (84.42%), Space: 19.1 MB (71.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        /*
-        // insertion sort concept
-        // TC: O(n^2)
-        // SC: O(1)
-        int zeroStart = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                int j = i - 1;
-                int val = nums[i];
-                for (; j >= zeroStart; j--) {
-                    nums[j + 1] = nums[j];
-                }
-                nums[j + 1] = val;
-                zeroStart++;
-            }
-        }
-        */
-        // two pointer
+        // Two pointer?
+        // Set zero pointer and forward pointer
         // TC: O(n)
         // SC: O(1)
-        int j = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
+        int pointer = 0;
+        for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                nums[j++] = nums[i];
+                swap(nums[pointer], nums[i]);
+                pointer++;
             }
-        }
-
-        // put zero at the end
-        for (int i = j; i < n; i++) {
-            nums[i] = 0;
         }
     }
 };
