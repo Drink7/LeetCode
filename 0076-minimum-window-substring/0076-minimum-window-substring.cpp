661065849029ca4ba3c8f433e35903b74eb15c77
Drink7 <johnnyliu505@gmail.com>
2023-08-16 00:49:43 +0800

    Time: 19 ms (68.81%), Space: 8 MB (55.86%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        // Sliding window
+        // TC: O(mn), m is s length, n is t length
+        // SC: O(mn)
+        if (s.size() < t.size()) {
+            return "";
+        }
+
+        unordered_map<char, int> tDict;
+        for (auto const& c : t) {
+            tDict[c]++;
+        }
+
+        int left = 0;
+        int right = 0;
+        int minStart = 0;
+        int minLen = INT_MAX;
+        int counter = t.size();
+
+        while (right < s.size()) {
+            char rightChar = s[right];
+            if (tDict[rightChar] > 0) {
+                counter--;
+            }
+            tDict[rightChar]--;
+
+            // Move left
+            while (counter == 0) {
+                char leftChar = s[left];
+                if (right - left + 1 < minLen) {
+                    minStart = left;
+                    minLen = right - left + 1;
+                }
+                tDict[leftChar]++;
+                if (tDict[leftChar] > 0) {
+                    counter++;
+                }
+                left++;
+            }
+
+            // Move right
+            right++;
+        }
+
+        return (minLen == INT_MAX) ? "" : s.substr(minStart, minLen);
+    }
+};
