ad0706823a5aedb084d37967754247f46c1eaad4
Drink7 <johnnyliu505@gmail.com>
2023-08-16 21:19:51 +0800

    Time: 115 ms (69.95%), Space: 97.3 MB (6.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // Sliding window
+        // dynamic rogramming
+        // calculate buy price to dp
+        // use prices - dp and check the max profit
         // TC: O(n)
-        // SC: O(1)
-        int left = 0;
-        int right = 0;
-        int profit = 0;
-        while (right < prices.size()) {
-            if (prices[right] < prices[left]) {
-                left = right;
-            }
-            profit = max(profit, prices[right] - prices[left]);
-            right++;
+        // SC: O(n)
+        vector<int> dp(prices.size(), 0);
+        dp[0] = prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            dp[i] = min(dp[i - 1], prices[i]);
         }
-        return profit;
+
+        // calculate max profit
+        int maxProfit = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            maxProfit = max(maxProfit, prices[i] - dp[i]);
+        }
+        return maxProfit;
     }
 };
