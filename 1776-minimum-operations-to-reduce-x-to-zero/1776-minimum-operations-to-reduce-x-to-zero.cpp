2190387df32e2adba6c4fefbe69311c00dc05972
Drink7 <johnnyliu505@gmail.com>
2023-08-18 20:16:40 +0800

    Time: 112 ms (96.91%), Space: 98.5 MB (73.45%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -5,29 +5,33 @@ public:
         // Find the longest subarray which sum value is sum(nums) - X
         // TC: O(n)
         // SC: O(1)
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        int remain = totalSum - x;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        int target = sum - x;
         int n = nums.size();
         int left = 0;
         int right = 0;
-        int sum = 0;
-        int longestResult = -1;
+        int curSum = 0;
+        int maxLen = -1;
         while (right < n) {
-            sum += nums[right];
-            while (left <= right && sum > remain) {
-                sum -= nums[left];
+            curSum += nums[right];
+            while (left <= right && curSum > target) {
+                curSum -= nums[left];
                 left++;
             }
-            if (sum == remain) {
-                longestResult = max(longestResult, right - left + 1);
+            if (curSum == target) {
+                maxLen = max(maxLen, right - left + 1);
             }
             right++;
         }
 
-        if (longestResult == -1) {
+        if (maxLen == -1) {
             return -1;
         } else {
-            return n - longestResult;
+            return n - maxLen;
         }
     }
 };
