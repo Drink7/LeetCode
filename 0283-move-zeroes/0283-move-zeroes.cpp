46eb8f9d8fb8d2b61df44369542ac141398b9578
Drink7 <johnnyliu505@gmail.com>
2023-08-18 15:26:52 +0800

    Time: 12 ms (96.41%), Space: 19.2 MB (32.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // Two pointer?
-        // Set zero pointer and forward pointer
+        // Two Pointer
         // TC: O(n)
         // SC: O(1)
-        int pointer = 0;
+        int index = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                swap(nums[pointer], nums[i]);
-                pointer++;
+                swap(nums[i], nums[index]);
+                index++;
             }
         }
     }
