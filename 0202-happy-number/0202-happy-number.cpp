65828650c571ae2099521d8826e2ebb3dcf06185
Drink7 <johnnyliu505@gmail.com>
2023-08-18 11:31:32 +0800

    Time: 3 ms (47.72%), Space: 6 MB (69.35%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isHappy(int n) {
+        /*
         // Use Hash Table
         // set
-        // TC: O(n)
-        // SC: O(n)
+        // TC: O(logn)
+        // SC: O(logn)
         unordered_set<int> numSet;
         while (n != 1) {
             if (numSet.count(n)) {
@@ -22,8 +23,37 @@ public:
             n = sum;
         }
         return true;
+        */
 
         // Two Pointer
+        // Floyd's Cycle-Finding Algorithm.
+        // TC: O(logn)
+        // SC: O(1)
+        int fast = n;
+        int slow = n;
+        fast = digitSum(fast);
+        fast = digitSum(fast);
+        slow = digitSum(slow);
+        while (fast != slow) {
+            fast = digitSum(fast);
+            fast = digitSum(fast);
+            slow = digitSum(slow);
+        }
 
+        if (slow == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    int digitSum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+        return sum;
     }
 };
