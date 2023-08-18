ebb033d6e75375983947fca598ba96803d84a2c4
Drink7 <johnnyliu505@gmail.com>
2023-08-18 22:01:56 +0800

    Time: 23 ms (21.72%), Space: 7 MB (37.29%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // Sliding window
-        // TC: O(n)
-        // SC: O(1)
+        /*
         // max char number + k >= current window size, it means we can replace all the distinct char in our window
         // else, it means we can not replace all the distinct char in the window
         // move left pointer
@@ -24,5 +22,33 @@ public:
             right++;
         }
         return right - left;
+        */
+        // Sliding window with more readable find max
+        // TC: O(n)
+        // SC: O(n)
+        // store the char freq in[left, right]
+        unordered_map<char, int> dict;
+        int n = s.size();
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        while (right < n) {
+            dict[s[right]]++;
+            while (findMaxCnt(dict) + k < right - left + 1) {
+                dict[s[left]]--;
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
+
+    int findMaxCnt(unordered_map<char, int>& dict) {
+        int maxCnt = INT_MIN;
+        for (auto const& p : dict) {
+            maxCnt = max(maxCnt, p.second);
+        }
+        return maxCnt;
     }
 };
