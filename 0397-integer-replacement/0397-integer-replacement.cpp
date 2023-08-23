002221de53b2055fccaeb74966eac4e39e86f9b7
Drink7 <johnnyliu505@gmail.com>
2023-08-23 21:19:05 +0800

    Time: 0 ms (100%), Space: 6 MB (48.62%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -4,7 +4,26 @@ public:
         // Recursion
         // TC: O(logn)
         // SC: O(1)
-        return replaceHelper(n);
+        //return replaceHelper(n);
+
+        // Bit operation
+        // if n end up with 01, use n - 1
+        // if n end up with 11, use n + 1
+        long bigN = n;
+        int count = 0;
+        while (bigN != 1) {
+            if ((bigN & 1) == 0) {
+                bigN = bigN >> 1;
+            } else {
+                if ((bigN & 2) == 0 || bigN == 3) {
+                    bigN = bigN - 1;
+                } else {
+                    bigN = bigN + 1;
+                }
+            }
+            count++;
+        }
+        return count;
     }
 
     int replaceHelper(long n) {
