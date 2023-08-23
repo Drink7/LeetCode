9ebb65f01edaf4c7e66dc23729ee444e04e30200
Drink7 <johnnyliu505@gmail.com>
2023-08-23 20:01:43 +0800

    Time: 0 ms (100%), Space: 6.1 MB (27%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        // use a uint32_t one to & n and find the number of 1
+        // Bit operation 32 times
         // TC: O(1)
         // SC: O(1)
-        int cnt = 0;
+        int count = 0;
         for (int i = 0; i < 32; i++) {
-            if (n & 1) {
-                cnt++;
+            if (n & 1 << i) {
+                count++;
             }
-            n = n >> 1;
         }
-        return cnt;
+        return count;
     }
 };
