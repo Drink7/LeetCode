0633b0dbb64c1042e4ddfc74dfb79d3c8106b433
Drink7 <johnnyliu505@gmail.com>
2023-08-27 13:49:04 +0800

    Time: 26 ms (98.44%), Space: 10.4 MB (22.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        // Two pointer, skip repeat
-        // First sort array, then calculate each abs(sum - target) to find the minimum
+        // sort first
+        // same 3 sum process but use abs to calculate closet
         // TC: O(n^2)
         // SC: O(1)
-        if (nums.size() < 3) {
-            return 0;
-        }
-
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        // avoid using INT_MAX here
-        int minSum = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < n; i++) {
-            // skip repeat
-            if (i > 0 && nums[i - 1] == nums[i]) {
+        int result = INT_MAX;
+        int minDiff = INT_MAX;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i >= 1 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int first = nums[i];
+
+            int num = nums[i];
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.size() - 1;
             while (left < right) {
-                int sum = first + nums[left] + nums[right];
-                if (abs(target - minSum) > abs(target - sum)) {
-                    minSum = sum;
-                }
-                if (sum == target) {
-                    return target;
-                } else if (sum > target) {
+                int sum = num + nums[left] + nums[right];
+                if (sum > target) {
                     right--;
-                    // skip repeat
-                    while (left < right && nums[right + 1] == nums[right]) {
-                        right--;
-                    }
-                } else {
+                } else if (sum < target) {
                     left++;
-                    // skip repeat
-                    while (left < right && nums[left -1] == nums[left]) {
-                        left++;
-                    }
+                } else {
+                    return sum;
+                }
+                
+                int diff = abs(sum - target);
+                if (minDiff > diff) {
+                    minDiff = diff;
+                    result = sum;
                 }
             }
         }
-        return minSum;
+        return result;
     }
 };
