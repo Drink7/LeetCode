807f5e5bb6763d605408cf11522a13a78592027b
Drink7 <johnnyliu505@gmail.com>
2023-08-27 16:49:22 +0800

    Time: 37 ms (95.24%), Space: 13.5 MB (36.37%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,105 +1,63 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        /*
-        // Sort first
-        // Same concept in 3Sum but would be O(n^3) here
+        // sort first
+        // use same concept of two sum
+        // careful of overflow
         // TC: O(n^3)
         // SC: O(1)
+        if (nums.size() < 4) {
+            return {};
+        }
+    
         sort(nums.begin(), nums.end());
-        int n = nums.size();
         vector<vector<int>> result;
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) {
+        for (int i = 0; i < nums.size() - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            long long int remain3Sum = target - nums[i];
+            long remain1 = target - nums[i];
 
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j - 1] == nums[j]) {
+            for (int j = i + 1; j < nums.size() - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
-                
-                long long int remain2Sum = remain3Sum - nums[j];
-                int left = j + 1;
-                int right = n - 1;
-                while (left < right) {
-                    long long int twoSum = nums[left] + nums[right];
-                    if (remain2Sum == twoSum) {
-                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
-                        left++;
-                        right--;
-                        // skip repeat
-                        while (left < right && nums[left - 1] == nums[left]) {
-                            left++;
-                        }
-                        // skip repeat
-                        while (left < right && nums[right + 1] == nums[right]) {
-                            right--;
-                        }
-                    } else if (remain2Sum < twoSum) {
-                        right--;
-                    } else {
-                        left++;
-                    }
+
+                long remain2 = remain1 - nums[j];
+                vector<vector<int>> twoSumList = twoSum(nums, j + 1, nums.size() - 1, remain2);
+                for (vector<int> v : twoSumList) {
+                    v.push_back(nums[i]);
+                    v.push_back(nums[j]);
+                    result.push_back(v);
                 }
             }
         }
         return result;
-        */
+    }
 
-        // More general, N-Sum (Base on two sum)
-        sort(nums.begin(), nums.end());
-        return NSum(nums, 0, nums.size() - 1, target, 4);
-    }
-    vector<vector<int>> NSum(vector<int>& nums, int left, int right, long long int target, int n) {
-        vector<vector<int>> results;
-        if (right - left + 1 < n || (long long int) nums[left] * n > target || (long long int) nums[right] * n < target) {
-            // if minimum possible sum (every element is first element) > target 
-            // or maximum possible sum (every element is first element) < target, it's impossible to get target anyway          
-            return results;
-        }
-        if (n == 2) {
-            return twoSum(nums, left, right, target);
-        } else {
-            for (int i = left; i < right; i++) {
-                if (i > left && nums[i - 1] == nums[i]) {
-                    continue;
-                }
-                int element = nums[i];
-                vector<vector<int>> resultFromNSum = NSum(nums, i + 1, right, target - element, n - 1);
-                for (vector<int> v : resultFromNSum) {
-                    v.push_back(element);
-                    results.push_back(v);
-                }
-            }
-        }
-        return results;
-    }
-    
-    vector<vector<int>> twoSum(vector<int>& nums, int left, int right, long long int target) {
+    vector<vector<int>> twoSum(vector<int>& nums, int left, int right, long long target) {
         vector<vector<int>> result;
-        
         while (left < right) {
-            long long int sum = nums[left] + nums[right];
+            int sum = nums[left] + nums[right];
             if (sum == target) {
                 result.push_back({nums[left], nums[right]});
                 left++;
                 right--;
 
                 // skip repeat
-                while (left < right && nums[left - 1] == nums[left]) {
+                while (left < right && nums[left] == nums[left - 1]) {
                     left++;
                 }
 
-                while (left < right && nums[right + 1] == nums[right]) {
+                // skip repeat
+                while (left < right && nums[right] == nums[right + 1]) {
                     right--;
                 }
-            } else if (sum < target) {
-                left++;
-            } else {
+            } else if (sum > target) {
                 right--;
+            } else {
+                left++;
             }
         }
         return result;
