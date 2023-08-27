abbe78dd0f085d06f0027298eb4692043c3be168
Drink7 <johnnyliu505@gmail.com>
2023-08-27 12:35:25 +0800

    Time: 8 ms (94.04%), Space: 6.9 MB (87.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        // Two pointer start from same position
-        // Move left and right pointer backward
+        // longest palindrome
+        // start from each letter, and expand left, right side to find longest length
+        // odd case and even case
         // TC: O(n^2)
         // SC: O(1)
-        int n = s.size();
-        int left = 0;
-        int right = 0;
-        int maxLength = 0;
+        int longestLen = 0;
+        int longestLenStart = 0;
         string result = "";
-
-        for (int i = 0; i < n; i++) {
-            left = i;
-            right = i;
-            // Odd
-            while (left >= 0 && right < n && s[left] == s[right]) {
+        for (int i = 0; i < s.size(); i++) {
+            // odd case 0 1 2
+            int left = i;
+            int right = i;
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
                 left--;
                 right++;
             }
-            if (right - left - 1 > maxLength) {
-                maxLength = right - left - 1;
-                result = s.substr(left + 1, right - left - 1);
+            
+            if (right - left + 1 - 2 > longestLen) {
+                longestLen = right - left + 1 - 2;
+                longestLenStart = left + 1;
             }
 
-            // Even
+            // even case 0 1 2 3
             left = i;
             right = i + 1;
-            while (left >= 0 && right < n && s[left] == s[right]) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
                 left--;
                 right++;
             }
-            if (right - left - 1 > maxLength) {
-                maxLength = right - left - 1;
-                result = s.substr(left + 1, right - left - 1);
+
+            if (right - left + 1 - 2 > longestLen) {
+                longestLen = right - left + 1 - 2;
+                longestLenStart = left + 1;
             }
         }
+
+        result = s.substr(longestLenStart, longestLen);
         return result;
     }
 };
