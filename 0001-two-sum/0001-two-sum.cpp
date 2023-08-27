53febb55f9385e3a6a74ffa2382f1ce69f0826b8
Drink7 <johnnyliu505@gmail.com>
2023-08-27 12:54:51 +0800

    Time: 12 ms (70.69%), Space: 10.8 MB (32.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,46 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        /*
-        // Hash table (unorder_map)
+        // hash table
         // TC: O(n)
         // SC: O(n)
-        // store val, id
         unordered_map<int, int> dict;
         for (int i = 0; i < nums.size(); i++) {
             int remain = target - nums[i];
             if (dict.count(remain)) {
                 return {dict[remain], i};
-            } else {
-                dict[nums[i]] = i;
             }
-        } 
-        return {-1, -1};
-        */
-        // Two pointer with sorted
-        // TC: O(nlogn)
-        // SC: O(n)
-        vector<pair<int, int>> numsArr;
-        for (int i = 0; i < nums.size(); i++) {
-            numsArr.push_back({nums[i], i});
+            dict[nums[i]] = i;
         }
-
-        sort(numsArr.begin(), numsArr.end(), [](pair<int, int>& a, pair<int, int>& b) {
-            return a.first < b.first;
-        });
-
-        int left = 0;
-        int right = numsArr.size() - 1;
-        while (left < right) {
-            int sum = numsArr[left].first + numsArr[right].first;
-            if (sum == target) {
-                return {numsArr[left].second, numsArr[right].second};
-            } else if (sum > target) {
-                right--;
-            } else {
-                left++;
-            }
-        }
+        // impossible
         return {};
     }
 };
