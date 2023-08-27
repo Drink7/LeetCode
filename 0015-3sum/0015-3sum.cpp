d2e5d0af9922fd540c5a55c0a8c9a6ec3a0a714c
Drink7 <johnnyliu505@gmail.com>
2023-08-27 13:16:53 +0800

    Time: 87 ms (96.14%), Space: 23.9 MB (86.11%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // Two pointer
-        // Sort array first
-        // Fix one element then do two pointer to sum the one element before and check we
-        // can get zero or not
+        // sort the nums first
+        // fix the first element, then use two pointer in the remain array
+        // note the repeated cases
         // TC: O(n^2)
         // SC: O(1)
         sort(nums.begin(), nums.end());
-        int n = nums.size();
         vector<vector<int>> result;
-        for (int i = 0; i < n; i++) {
-            // ignore duplicate element
-            if (i > 0 && nums[i - 1] == nums[i]) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (i >= 1 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int first = nums[i];
+            int num = nums[i];
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.size() - 1;
             while (left < right) {
-                int internalSum = nums[left] + nums[right];
-                if (first + internalSum == 0) {
-                    result.push_back({first, nums[left], nums[right]});
+                int sum = num + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.push_back({num, nums[left], nums[right]});
                     left++;
                     right--;
-                    // ignore duplicate element
-                    while (left < right && nums[left - 1] == nums[left]) {
+
+                    while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
 
-                    // ignore duplicate element
-                    while (left < right && nums[right + 1] == nums[right]) {
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
-                } else if (first + internalSum > 0) {
-                    right--;
-                } else {
-                    left++;
                 }
             }
         }
