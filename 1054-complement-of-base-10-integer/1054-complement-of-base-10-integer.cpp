31b009b77b5b3c96ec725f68fc035c0b54a5ad04
Drink7 <johnnyliu505@gmail.com>
2023-08-30 00:05:33 +0800

    Time: 2 ms (42.77%), Space: 5.9 MB (49.23%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        // use XOR to original num
+        // TC: O(1)
+        // SC: O(1)
+        // exception handle first
+        if (n == 0) {
+            return 1;
+        }
+
+        // find leading one
+        int totalBit = 0;
+        int tmp = n;
+        while (tmp) {
+            tmp = tmp >> 1;
+            totalBit++;
+        }
+
+        for (int i = 0; i < totalBit; i++) {
+            n = n ^ (1 << i);
+        }
+
+        return n;
+    }
+};
