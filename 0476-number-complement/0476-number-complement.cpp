52930abf262e144b0acedd9c3568f2060506de35
Drink7 <johnnyliu505@gmail.com>
2023-08-30 00:17:55 +0800

    Time: 2 ms (43.71%), Space: 6.1 MB (23.98%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -25,10 +25,10 @@ public:
         */
 
         // Elegant
-        unsigned mask = ~0;
-        while (num & mask) {
-            mask = mask << 1;
+        int mask = 1;
+        while (mask < num) {
+            mask = (mask << 1) | 1;
         }
-        return ~mask ^ num;
+        return mask ^ num;
     }
 };
