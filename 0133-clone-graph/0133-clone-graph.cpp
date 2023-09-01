f4cd791765a2b81d6f5ead36ed2a0fe91046d839
Drink7 <johnnyliu505@gmail.com>
2023-09-01 23:06:36 +0800

    Time: 3 ms (82.82%), Space: 8.6 MB (83.37%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -61,15 +61,12 @@ public:
                 Node* n = q.front();
                 q.pop();
                 for (auto const& neighbor : n->neighbors) {
-                    Node* cloneNeighbor;
-                    if (cloneDict.count(neighbor->val)) {
-                        cloneNeighbor = cloneDict[neighbor->val];
-                    } else {
-                        cloneNeighbor = new Node(neighbor->val);
-                        cloneDict[cloneNeighbor->val] = cloneNeighbor;
+                    if (cloneDict.count(neighbor->val) == 0) {
+                        Node* cloneNeighbor = new Node(neighbor->val);
+                        cloneDict[neighbor->val] = cloneNeighbor;
                         q.push(neighbor);
                     }
-                    cloneDict[n->val]->neighbors.push_back(cloneNeighbor);
+                    cloneDict[n->val]->neighbors.push_back(cloneDict[neighbor->val]);
                 }
             }
         }
