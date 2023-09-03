7e4b3e145fe707fab3d8d42ee062682594448e00
Drink7 <johnnyliu505@gmail.com>
2023-09-03 22:16:10 +0800

    Time: 15 ms (72.41%), Space: 17 MB (34.14%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -5,7 +5,7 @@ public:
         // TC: O(n)
         // SC: O(1)
         // Solution 2: set (insert the set then erase the element in set)
-        // Solution 3: 
+        // Solution 3: set + math // 2 (a + b + c) - (a + a + b + b + c) = c
         int base = nums[0];
         for (int i = 1; i < nums.size(); i++) {
             base = base ^ nums[i];
