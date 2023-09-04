612993dcbbe2b1a283b1f82d99c979e5c34a7497
Drink7 <johnnyliu505@gmail.com>
2023-09-04 21:21:19 +0800

    Time: 15 ms (34.13%), Space: 15.8 MB (69.17%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        /*
         // hash table
         // TC: O(n)
         // SC: O(n)
@@ -14,6 +15,53 @@ public:
             if (p.second > nums.size() / 3) {
                 result.push_back(p.first);
             }
+        }
+        return result;
+        */
+        // Similar to Majority Element but with two counter and candidate
+        // TC: O(n)
+        // SC: O(1)
+        vector<int> result;
+        int count1 = 0;
+        int count2 = 0;
+        int candidate1 = 0;
+        int candidate2 = 1;
+        for (auto const& num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else {
+                if (count1 == 0) {
+                    candidate1 = num;
+                    count1++;
+                } else if (count2 == 0) {
+                    candidate2 = num;
+                    count2++;
+                } else {
+                    count1--;
+                    count2--;
+                }
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (auto const& num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        if (count1 > nums.size() / 3) {
+            result.push_back(candidate1);
+        }
+
+        if (count2 > nums.size() / 3) {
+            result.push_back(candidate2);
         }
         return result;
     }
