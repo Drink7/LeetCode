256cb998495dbaa3d85914aabf60737d6c1d0f0c
Drink7 <johnnyliu505@gmail.com>
2023-09-05 23:39:16 +0800

    Time: 86 ms (90.96%), Space: 93.4 MB (12.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        /*
         // find a min value array
         // maintain a max profit value
         // TC: O(n)
@@ -9,12 +10,25 @@ public:
         int profit = 0;
         minPrices[0] = prices[0];
         for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] < minPrices[i - 1]) {
-                minPrices[i] = prices[i];
-            } else {
-                minPrices[i] = minPrices[i - 1];
-            }
+            minPrices[i] = (prices[i] < minPrices[i - 1]) ? prices[i] : minPrices[i - 1];
             profit = max(profit, prices[i] - minPrices[i]);
+        }
+        return profit;
+        */
+        // two pointer
+        // sliding windows
+        // TC: O(n)
+        // SC: O(1)
+        int left = 0;
+        int right = 0;
+        int profit = 0;
+        while (right < prices.size()) {
+            int sellPrice = prices[right];
+            if (sellPrice < prices[left]) {
+                left = right;
+            }
+            profit = max(profit, sellPrice - prices[left]);
+            right++;
         }
         return profit;
     }
