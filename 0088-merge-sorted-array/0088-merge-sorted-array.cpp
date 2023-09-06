6a0e2bdfa74c4dbd3cfd2f0435b0492045c86e5e
Drink7 <johnnyliu505@gmail.com>
2023-09-06 21:57:04 +0800

    Time: 0 ms (100%), Space: 9.1 MB (46.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,12 +1,38 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // Cheating STL answer (Array + sorting)
-        // TC: O(mlogm + n)
-        // SC: O(1)
-        for (int i = 0; i < n; i++) {
-            nums1[i + m] = nums2[i];
+        // use a copy array of nums1 with m size
+        // TC: O(2m + n)
+        // SC: O(m)
+        vector<int> copyNums1(m);
+        copyNums1.assign(nums1.begin(), nums1.begin() + m);
+
+        // merge copyNums1 and nums2 to nums1
+        // merge sort
+        int n1 = 0;
+        int n2 = 0;
+        int index = 0;
+        while (n1 < m && n2 < n) {
+            if (copyNums1[n1] > nums2[n2]) {
+                nums1[index] = nums2[n2];
+                n2++;
+            } else {
+                nums1[index] = copyNums1[n1];
+                n1++;
+            }
+            index++;
         }
-        sort(nums1.begin(), nums1.end());
+
+        while (n1 < m) {
+            nums1[index] = copyNums1[n1];
+            n1++;
+            index++;
+        }
+
+        while (n2 < n) {
+            nums1[index] = nums2[n2];
+            n2++;
+            index++;
+        }
     }
 };
