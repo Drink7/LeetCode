e2f429f0211d8644d8f889d2790a971457161f3f
Drink7 <johnnyliu505@gmail.com>
2023-09-06 22:01:34 +0800

    Time: 0 ms (100%), Space: 9 MB (46.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        /*
         // use a copy array of nums1 with m size
         // TC: O(2m + n)
         // SC: O(m)
@@ -33,6 +34,36 @@ public:
             nums1[index] = nums2[n2];
             n2++;
             index++;
+        }
+        */
+
+        // Sort it from the end
+        // TC: O(m + n)
+        // SC: O(1)
+        int n1 = m - 1;
+        int n2 = n - 1;
+        int index = m + n - 1;
+        while (index >= 0) {
+            if (n1 >= 0 && n2 >= 0) {
+                if (nums1[n1] > nums2[n2]) {
+                    nums1[index] = nums1[n1];
+                    n1--;
+                } else {
+                    nums1[index] = nums2[n2];
+                    n2--;
+                }
+            } else {
+                if (n1 >= 0) {
+                    nums1[index] = nums1[n1];
+                    n1--;
+                }
+
+                if (n2 >= 0) {
+                    nums1[index] = nums2[n2];
+                    n2--;
+                }
+            }
+            index--;
         }
     }
 };
