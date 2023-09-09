1c848a09244772dc4063e4135193d917d29e5be9
Drink7 <johnnyliu505@gmail.com>
2023-09-10 01:45:18 +0800

    Time: 27 ms (50.26%), Space: 23.4 MB (22.01%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,30 +11,36 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // make sure p->val < q->val
-        // check root->val
+        /*
+        // check root->val (Recursive)
         // TC: O(logn)
         // SC: O(logn) -> recursion stack
         if (root == nullptr) {
             return root;
         }
 
-        if (p->val > q->val) {
-            return lowestCommonAncestor(root, q, p);
-        }
-
-        if (p->val < root->val && q->val > root->val) {
-            return root;
-        } else if (p->val <= root->val && q->val > root->val) {
-            return p;
-        } else if (p->val < root->val && q->val >= root->val) {
-            return q;
+        if (p->val > root->val && q->val > root->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else if (p->val < root->val && q->val < root->val){
+            return lowestCommonAncestor(root->left, p, q);
         } else {
-            if (p->val > root->val) {
-                return lowestCommonAncestor(root->right, p, q);
+            return root;
+        }
+        */
+
+        // check root->val (Iterative)
+        // TC: O(logn)
+        // SC: O(1) -> recursion stack
+        TreeNode* cur = root;
+        while (cur != nullptr) {
+            if (p->val > cur->val && q->val > cur->val) {
+                cur = cur->right;
+            } else if (p->val < cur->val && q->val < cur->val) {
+                cur = cur->left;
             } else {
-                return lowestCommonAncestor(root->left, p, q);
+                break;
             }
         }
+        return cur;
     }
 };
