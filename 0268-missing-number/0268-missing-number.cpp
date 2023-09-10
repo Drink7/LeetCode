36fabe0e06746c1420ca5d71c023ec084c5e1807
Drink7 <johnnyliu505@gmail.com>
2023-09-10 21:46:48 +0800

    Time: 10 ms (92.44%), Space: 17.9 MB (91.64%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // XOR number itself would be zero
-        // 0 0 0
-        // 1 0 1
-        // 0 1 1
-        // 1 1 0
+        // XOR from 0 to n
         // TC: O(n)
         // SC: O(1)
         int n = nums.size();
-        int result = n;
-        for (int i = 0; i < nums.size(); i++) {
-            result = (result ^ i ^ nums[i]);
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = result ^ nums[i];
+        }
+
+        for (int i = 0; i <= n; i++) {
+            result = result ^ i;
         }
         return result;
     }
