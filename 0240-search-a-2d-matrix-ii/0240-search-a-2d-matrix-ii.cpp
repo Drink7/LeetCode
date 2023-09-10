7630806856efa1d9493ea7fdf7eb8aebe3dce0c8
Drink7 <johnnyliu505@gmail.com>
2023-09-11 00:21:59 +0800

    Time: 88 ms (69.39%), Space: 14.8 MB (90.09%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        /*
         // search the target by row
         // each row would use binary search
         // TC: O(m * log n)
@@ -19,6 +20,29 @@ public:
                 } else {
                     left = mid + 1;
                 }
+            }
+        }
+        return false;
+        */
+
+        // checking top-right
+        // if target > top-right
+        // no need to check the top row, row = row - 1
+        // if target < top-right
+        // no need to check the last col, col = col - 1
+        // TC: O(m * n)
+        // SC: O(1)
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int curRow = 0;
+        int curCol = col - 1;
+        while (curRow < row && curCol >= 0) {
+            if (matrix[curRow][curCol] == target) {
+                return true;
+            } else if (matrix[curRow][curCol] < target) {
+                curRow++;
+            } else {
+                curCol--;
             }
         }
         return false;
