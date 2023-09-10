f7c9b3805bfeb99a21ba5d6b803307e1b7b4d412
Drink7 <johnnyliu505@gmail.com>
2023-09-10 22:02:50 +0800

    Time: 2 ms (53.41%), Space: 6 MB (14.25%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,25 +4,24 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        // all the versions after a bad version are also bad
-        // Find the first element that is bad version
+        // find the first target -> BS
         // TC: O(logn)
         // SC: O(1)
-        int left = 1;
-        int right = n;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            bool isVersionBad = isBadVersion(mid);
-            if (!isVersionBad) {
-                left = mid + 1;
-            } else {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) {
                 if (mid == 1 || !isBadVersion(mid - 1)) {
                     return mid;
                 } else {
-                    right = mid - 1;
+                    high = mid - 1;
                 }
+            } else {
+                low = mid + 1;
             }
         }
+        // impossible
         return -1;
     }
 };
