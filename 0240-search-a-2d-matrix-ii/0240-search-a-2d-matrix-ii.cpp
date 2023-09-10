46ec8c0e818ba9c19216c3a0668928d58e06e13a
Drink7 <johnnyliu505@gmail.com>
2023-09-11 00:13:32 +0800

    Time: 285 ms (13.37%), Space: 14.8 MB (90.09%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        /*
-        // Binary Search way
-        // TC: O(mlogn)
+        // search the target by row
+        // each row would use binary search
+        // TC: O(m * log n)
         // SC: O(1)
-        int m = matrix.size();
-        int n = matrix[0].size();
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] <= target) {
-                int left = 0;
-                int right = n - 1;
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-                    if (matrix[i][mid] == target) {
-                        return true;
-                    } else if (matrix[i][mid] > target) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }
+        int row = matrix.size();
+        int col = matrix[0].size();
+        for (int i = 0; i < row; i++) {
+            int left = 0;
+            int right = col - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (matrix[i][mid] == target) {
+                    return true;
+                } else if (matrix[i][mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
-            } else {
-                break;
-            }
-        }
-        return false;
-        */
-
-        // Rule out each row or col by checking top right
-        int row = 0;
-        int col = matrix[0].size() - 1;
-        while (row < matrix.size() && col >= 0) {
-            if (matrix[row][col] == target) {
-                return true;
-            } else if (matrix[row][col] > target) {
-                col--;
-            } else {
-                row++;
             }
         }
         return false;
