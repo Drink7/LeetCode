6942cb19ae30ade327f774fcc75b919f0f64e6cf
Drink7 <johnnyliu505@gmail.com>
2023-09-11 00:47:00 +0800

    Time: 7 ms (18.31%), Space: 9.7 MB (10.19%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // Find row then find col
+        // first find the row value which is closet (smaller) to the target
+        // then keep the same row, find the value in [0, col - 1] range
         // TC: O(logm + logn)
         // SC: O(1)
-        int left = 0;
-        int right = matrix.size() - 1;
-        int row = findRow(matrix, left, right, target);
-        if (row == -1) {
-            return false;
-        } else {
-            left = 0;
-            right = matrix[0].size() - 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (matrix[row][mid] == target) {
-                    return true;
-                } else if (matrix[row][mid] > target) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
+        int minRow = findRow(matrix, target);
+        if (minRow == -1) {
             return false;
         }
-    }
-
-    int findRow(vector<vector<int>>& matrix, int l, int r, int target) {
-        int left = l;
-        int right = r;
+    
+        int left = 0;
+        int right = matrix[0].size() - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            // find the last smaller or equal to target
+            if (matrix[minRow][mid] == target) {
+                return true;
+            } else if (matrix[minRow][mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    int findRow(vector<vector<int>>& matrix, int& target) {
+        int row = matrix.size();
+        int left = 0;
+        int right = row - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (matrix[mid][0] <= target) {
-                if (mid == matrix.size() - 1 || matrix[mid + 1][0] > target) {
+                if (mid == right || matrix[mid + 1][0] > target) {
                     return mid;
                 } else {
                     left = mid + 1;
