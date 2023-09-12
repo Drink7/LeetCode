0668e99686bea81f0e624871c79efb68b5bf31bd
Drink7 <johnnyliu505@gmail.com>
2023-09-12 22:55:11 +0800

    Time: 350 ms (11.62%), Space: 135.8 MB (11.97%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        // TC: O(2^n)
+        // SC: O(2^n)
+        vector<vector<int>> result;
+        vector<int> combination;
+        combineHelper(result, combination, 1, n, k);
+        return result;
+    }
+
+    void combineHelper(vector<vector<int>>& result, vector<int> combination, int index, int n, int k) {
+        if (combination.size() == k) {
+            result.push_back(combination);
+            return;
+        }
+
+        for (int i = index; i <= n; i++) {
+            combination.push_back(i);
+            combineHelper(result, combination, i + 1, n, k);
+            combination.pop_back();
+        }
+    }
+};
