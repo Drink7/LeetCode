cdc12655a026183ac28ead89271f5da29b71c84f
Drink7 <johnnyliu505@gmail.com>
2023-09-13 21:10:46 +0800

    Time: 8 ms (39.08%), Space: 7.4 MB (6.57%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        // Two hash table mapping
+        // two hash tables
         // TC: O(n)
         // SC: O(n)
         if (s.size() != t.size()) {
             return false;
         }
 
-        unordered_map<char, char> sDict, tDict;
-
+        unordered_map<char, char> sDict;
+        unordered_map<char, char> tDict;
         for (int i = 0; i < s.size(); i++) {
             sDict[s[i]] = t[i];
             tDict[t[i]] = s[i];
         }
 
-        for (int i = 0; i < s.size(); i++) {
-            if (sDict[s[i]] != t[i] || tDict[t[i]] != s[i]) {
-                return false;   
+        for (int i = 0; i < t.size(); i++) {
+            if (tDict[t[i]] != s[i] || sDict[s[i]] != t[i]) {
+                return false;
             }
         }
         return true;
