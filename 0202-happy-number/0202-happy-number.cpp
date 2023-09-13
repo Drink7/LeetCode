bbec1e46199a0a2543fe648586d8dfd1f7477b5c
Drink7 <johnnyliu505@gmail.com>
2023-09-13 20:15:43 +0800

    Time: 0 ms (100%), Space: 6.6 MB (9.54%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,58 +1,25 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        /*
-        // Use Hash Table
-        // set
-        // TC: O(logn)
-        // SC: O(logn)
-        unordered_set<int> numSet;
+        // use unordered_set to store calculated numbers
+        unordered_set<int> happySet;
         while (n != 1) {
-            if (numSet.count(n)) {
+            n = digitHelper(n);
+            if (happySet.count(n)) {
                 return false;
             } else {
-                numSet.insert(n);
+                happySet.insert(n);
             }
-
-            int sum = 0;
-            while (n != 0) {
-                int digit = n % 10;
-                sum += digit * digit;
-                n = n / 10;
-            }
-            n = sum;
         }
         return true;
-        */
-
-        // Two Pointer
-        // Floyd's Cycle-Finding Algorithm.
-        // TC: O(logn)
-        // SC: O(1)
-        int fast = n;
-        int slow = n;
-        fast = digitSum(fast);
-        fast = digitSum(fast);
-        slow = digitSum(slow);
-        while (fast != slow) {
-            fast = digitSum(fast);
-            fast = digitSum(fast);
-            slow = digitSum(slow);
-        }
-
-        if (slow == 1) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
-    int digitSum(int n) {
+    int digitHelper(int x) {
         int sum = 0;
-        while (n != 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n = n / 10;
+        while (x != 0) {
+            int remain = x % 10;
+            x = x / 10;
+            sum += remain * remain;
         }
         return sum;
     }
