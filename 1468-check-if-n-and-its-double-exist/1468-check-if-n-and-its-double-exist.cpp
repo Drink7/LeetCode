2a6e75dc6afc5534d7eb88592fae63a26bb986b3
Drink7 <johnnyliu505@gmail.com>
2023-09-17 17:26:56 +0800

    Time: 4 ms (82.1%), Space: 10.7 MB (19.73%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
+        /*
         // Brute Force
         // TC: O(n^2)
         // SC: O(1)
@@ -10,6 +11,24 @@ public:
                     return true;
                 }
             }
+        }
+        return false;
+        */
+
+        // use hash set
+        // TC: O(n)
+        // SC: O(n)
+        unordered_set<int> numSet;
+        for (auto const& num : arr) {
+            int doubleNum = 2 * num;
+            if (numSet.count(doubleNum)) {
+                return true;
+            }
+
+            if (num % 2 == 0 && numSet.count(num / 2)) {
+                return true;
+            }
+            numSet.insert(num);
         }
         return false;
     }
