23cc3c48eb3d0e889df9be54d875c3e3f88252e1
Drink7 <johnnyliu505@gmail.com>
2023-09-17 17:04:56 +0800

    Time: 0 ms (100%), Space: 6.3 MB (16.59%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -6,8 +6,10 @@ public:
         // SC: O(1)
         int cnt = 0;
         while (num != 0) {
-            if (num % 2) {
-                num -= 1;
+            if (num & 1) {
+            //if (num % 2) {
+                //num -= 1;
+                num = num ^ 1;
             } else {
                 num = num >> 1;
             }
