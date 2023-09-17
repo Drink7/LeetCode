bef2898d4b5851c6cc3257a36cfb105e90ab1cb7
Drink7 <johnnyliu505@gmail.com>
2023-09-18 00:27:43 +0800

    Time: 59 ms (21.18%), Space: 19.9 MB (73.39%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        // use two pointer
-        // if there is one not match, check right - 1 and left + 1 cases
+        // two pointer
         // TC: O(n)
         // SC: O(1)
         int left = 0;
         int right = s.size() - 1;
-        while (left <= right) {
+        while (left < right) {
             if (s[left] != s[right]) {
-                return isPalindromeHelper(s, left + 1, right) ||
-                        isPalindromeHelper(s, left, right - 1);
-            } else {
-                left++;
-                right--;
+                return validHelper(s, left + 1, right) || validHelper(s, left, right - 1);
             }
+            left++;
+            right--;
         }
         return true;
     }
 
-    bool isPalindromeHelper(string s, int left, int right) {
-        while (left <= right) {
+    bool validHelper(string& s, int left, int right) {
+        while (left < right) {
             if (s[left] != s[right]) {
                 return false;
-            } else {
-                left++;
-                right--;
             }
+            left++;
+            right--;
         }
         return true;
     }
