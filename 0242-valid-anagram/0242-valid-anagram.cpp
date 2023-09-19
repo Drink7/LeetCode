d6d11474b2b34865801f903596dbc1d89a9b3794
Drink7 <johnnyliu505@gmail.com>
2023-09-19 22:35:08 +0800

    Time: 7 ms (77.55%), Space: 8 MB (5.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        /*
         // sort and compare
         // TC: O(nlogn)
         // SC: O(1)
@@ -14,6 +15,27 @@ public:
             if (s[i] != t[i]) {
                 return false;
             }
+        }
+        return true;
+        */
+        // Use hash map to check valid
+        // store s to check t
+        // TC: O(n)
+        // SC: O(n)
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        unordered_map<char, int> letterCnt;
+        for (auto const& c : s) {
+            letterCnt[c]++;
+        }
+
+        for (auto const& c : t) {
+            if (letterCnt[c] == 0) {
+                return false;
+            }
+            letterCnt[c]--;
         }
         return true;
     }
