5d3fb2df5e47ffdd93dd74597424c11b13d48a9a
Drink7 <johnnyliu505@gmail.com>
2023-09-19 22:23:06 +0800

    Time: 20 ms (86.72%), Space: 11 MB (11.93%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        // hash map
-        unordered_map<char, int> letterMap;
-        vector<int> letterIndex(26, INT_MAX);
+        // hash
+        // TC: O(n)
+        // SC: O(1)
+        vector<int> letterIndex(26, 0);
         for (int i = 0; i < s.size(); i++) {
-            if (letterMap.count(s[i]) < 1) {
-                letterIndex[s[i] - 'a'] = i;
-            }
-            letterMap[s[i]]++;
+            letterIndex[s[i] - 'a']++;
         }
 
-        int result = INT_MAX;
-        for (auto const& p : letterMap) {
-            char letter = p.first;
-            int letterCnt = p.second;
-            if (letterCnt == 1) {
-                if (result > letterIndex[letter - 'a']) {
-                    result = letterIndex[letter - 'a'];
-                }
+        for (int i = 0; i < s.size(); i++) {
+            if (letterIndex[s[i] - 'a'] == 1) {
+                return i;
             }
         }
-        return (result == INT_MAX) ? -1 : result;
+        return -1;
     }
 };
