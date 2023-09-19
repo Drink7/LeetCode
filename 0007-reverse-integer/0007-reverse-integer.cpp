7e8b068015b35795d60ab8558fd61d5a20860675
Drink7 <johnnyliu505@gmail.com>
2023-09-19 22:28:25 +0800

    Time: 2 ms (41.18%), Space: 6.3 MB (7.54%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int reverse(int x) {
         // Math
-        // TC: O(n)
+        // TC: O(n), n is the digit number
         // SC: O(1)
         int res = 0;
         while (x != 0) {
