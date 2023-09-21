19e55d1c01bf5f35712cba11ec93ba55081accaa
Drink7 <johnnyliu505@gmail.com>
2023-09-22 00:38:26 +0800

    Time: 0 ms (100%), Space: 10.5 MB (5.64%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,62 +1,24 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        /*
-        // O(n) solution
-        // TC: O(n)
+        // binary search
+        // TC: O(logn)
         // SC: O(1)
-        if (nums[0] <= nums.back()) {
-            return nums[0];
-        }
 
-        for (auto const& num : nums) {
-            if (num < nums[0]) {
-                return num;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= nums[right]) {
+                if (mid == 0 || nums[mid - 1] > nums[mid]) {
+                    return nums[mid];
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                left = mid + 1;
             }
         }
         return -1;
-        */
-
-        /*
-        // num[mid] > num[right]
-        // smaller part is on the right
-        // num[mid] < num[right]
-        // smaller part is on the left
-        // TC: O(logn)
-        // SC: O(1)
-        int left = 0;
-        int right = nums.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < nums[(mid + 1) % nums.size()] && nums[mid] < nums[(mid + nums.size() - 1) % nums.size()]) {
-                return nums[mid];
-            } else if (nums[mid] > nums[right]) {
-               left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return nums[left];
-        */
-
-        // num[mid] > num[right]
-        // smaller part is on the right
-        // num[mid] < num[right]
-        // smaller part is on the left
-        // TC: O(logn)
-        // SC: O(1)
-        int left = 0;
-        int right = nums.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (left == right) {
-                return nums[mid];
-            } else if (nums[mid] > nums[right]) {
-               left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return nums[left];
     }
 };
