93f4932bf42003b44b0c9588a8212d2c60c337a9
Drink7 <johnnyliu505@gmail.com>
2023-09-22 00:50:26 +0800

    Time: 4 ms (27.35%), Space: 11.4 MB (7.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // Similar to LC 153.
+        // binary search
         // TC: O(logn)
         // SC: O(1)
         int left = 0;
@@ -10,19 +10,20 @@ public:
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] > nums[right]) {
-                // Array must be rotated
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
             } else {
-                // Array might be rotated
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
+                if (nums[mid] <= nums[right]) {
+                    // right part still sorted
+                    if (target > nums[mid] && target <= nums[right]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
                 } else {
-                    right = mid - 1;
+                    if (target >= nums[left] && target < nums[mid]) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
                 }
             }
         }
