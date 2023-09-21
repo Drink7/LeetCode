39127b0c06157ab3665a1bcb6399ced41195260a
Drink7 <johnnyliu505@gmail.com>
2023-09-21 22:56:45 +0800

    Time: 20 ms (45.72%), Space: 14.2 MB (38.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,89 +1,48 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        /*
-        vector<bool> visited(numCourses);
-        for (int i = 0; i < prerequisites.size(); i++) {
-            int curCourse = prerequisites[i][0];
-            int preCourse = prerequisites[i][1];
-            courseMap[curCourse].push_back(preCourse);
-        }
-
-        for (auto const& course : courseMap) {
-            int curCourse = course.first;
-            if (!dfs(visited, curCourse)) {
-                return false;
-            }
-        }
-        return true;
-        */
-        // Init the graph
-        vector<int> inDegree(numCourses, 0);
+        // topological sort
+        // TC: O(V + E)
+        // SC: O(V + E)
         unordered_map<int, vector<int>> graph;
-        for(int i = 0; i < numCourses; ++i) {
-            inDegree[i] = 0;
-            graph[i] = {};
+        vector<int> inDegree(numCourses, 0);
+
+        // build graph
+        for (auto const& prerequisite : prerequisites) {
+            int preq = prerequisite[1];
+            int cur = prerequisite[0];
+            graph[preq].push_back(cur);
+            inDegree[cur]++;
         }
 
-        // Build the graph
-        for(int i = 0; i < prerequisites.size(); ++i) {
-            int prereq = prerequisites[i][1];
-            int course = prerequisites[i][0];
+        // bfs queue
+        queue<int> q;
+        int count = 0;
 
-            graph[prereq].push_back(course);
-            inDegree[course]++;
-        }
-
-        // Find all sources
-        queue<int> sources;
-        for(int i = 0; i < numCourses; i++) {
-            if(inDegree[i] == 0) {
-                sources.push(i);
+        // put unrelated
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        // Build the sorted order
-        vector<int> sortedCourse;
-        while(!sources.empty()) {
-            int course = sources.front();
-            sources.pop();
+        while (!q.empty()) {
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                int course = q.front();
+                q.pop();
 
-            sortedCourse.push_back(course);
+                count++;
 
-            for(auto& child: graph[course]) {
-                if(--inDegree[child] == 0) {
-                    sources.push(child);
+                for (int j = 0; j < graph[course].size(); j++) {
+                    int next = graph[course][j];
+                    inDegree[next]--;
+                    if (inDegree[next] == 0) {
+                        q.push(next);
+                    }
                 }
             }
         }
-
-        // If the size of topological order list is not equal to numCourses
-        // Return false
-        return sortedCourse.size() == numCourses;
+        return count == numCourses;
     }
-
-    bool dfs(vector<bool>& visited, int course) {
-        // No prerequisite
-        if (courseMap.count(course) < 1) {
-            return true;
-        }
-
-        if (visited[course]) {
-            return false;
-        }
-
-        visited[course] = true;
-        vector<int> preCourses = courseMap[course];
-        int isFinish = true;
-        for (int i = 0; i < preCourses.size(); i++) {
-            isFinish = isFinish && dfs(visited, preCourses[i]);
-        }
-        // prune
-        courseMap[course] = {};
-        visited[course] = false;
-        return isFinish;
-    }
-
-private:
-    unordered_map<int, vector<int>> courseMap;
 };
