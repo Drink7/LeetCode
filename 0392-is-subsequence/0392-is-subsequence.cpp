f85f82631e171c6f1e19bb0a939c6a554e73006d
Drink7 <johnnyliu505@gmail.com>
2023-09-22 22:12:23 +0800

    Time: 3 ms (49.1%), Space: 6.6 MB (17.6%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        /*
-        // TC: O(n), n is t length
-        // SC: O(1)
-        int sId = 0;
-        int tId = 0;
-        while (tId < t.size()) {
-            if (s[sId] == t[tId]) {
-                sId++;
-            }
-            tId++;
-        }
-        return sId == s.size();
-        */
-
-        // recursion
         // TC: O(n)
-        // SC: O(n)
-        int sLen = s.size();
-        int tLen = s.size();
-        return isSubseq(s, t, 0, 0);
-    }
-
-    bool isSubseq(string& s, string& t, int x, int y) {
-        if (x == s.size()) {
-            return true;
+        // SC: O(1)
+        int sIndex = 0;
+        int tIndex = 0;
+        while (tIndex < t.size()) {
+            if (s[sIndex] == t[tIndex]) {
+                sIndex++;
+            }
+            tIndex++;
         }
-
-        if (y == t.size()) {
-            return false;
-        }
-
-        if (s[x] == t[y]) {
-            return isSubseq(s, t, x + 1, y + 1);
-        } else {
-            return isSubseq(s, t, x, y + 1);
-        }
+        return sIndex == s.size();
     }
 };
