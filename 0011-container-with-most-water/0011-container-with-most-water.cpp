5baea2080127877b41917d2219288f2dcba642dd
Drink7 <johnnyliu505@gmail.com>
2023-09-24 13:42:00 +0800

    Time: 79 ms (16.84%), Space: 59.5 MB (5.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // two pointer and move the smaller height side to find max volumn
+        // two pointer
+        // move smaller height size
         // TC: O(n)
         // SC: O(1)
-        int result = 0;
         int left = 0;
         int right = height.size() - 1;
+        int result = 0;
         while (left < right) {
-            int tmp = min(height[left], height[right]) * (right - left);
-            result = max(result, tmp);
+            int area = min(height[left], height[right]) * (right - left);
+            result = max(result, area);
+
             if (height[left] > height[right]) {
                 right--;
             } else {
