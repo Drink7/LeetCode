9bc9a549bdda7f5ba84271f780df48190507013b
Drink7 <johnnyliu505@gmail.com>
2023-09-24 13:32:40 +0800

    Time: 84 ms (94.33%), Space: 24.3 MB (66.6%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // sort the nums first
-        // fix the first element, then use two pointer in the remain array
-        // note the repeated cases
+        // two pointer
         // TC: O(n^2)
         // SC: O(1)
+
+        // sort first
         sort(nums.begin(), nums.end());
+
         vector<vector<int>> result;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i >= 1 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int num = nums[i];
-            int left = i + 1;
-            int right = nums.size() - 1;
-            while (left < right) {
-                int sum = num + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    result.push_back({num, nums[left], nums[right]});
-                    left++;
-                    right--;
-
-                    while (left < right && nums[left] == nums[left - 1]) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                int target = 0 - nums[i];
+                int left = i + 1;
+                int right = n - 1;
+                while (left < right) {
+                    int sum = nums[left] + nums[right];
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[left], nums[right]});
+                        
                         left++;
-                    }
-
-                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
+                        
+                        // skip left
+                        while (left < right && nums[left - 1] == nums[left]) {
+                            left++;
+                        }
+
+                        // skip right
+                        while (left < right && nums[right + 1] == nums[right]) {
+                            right--;
+                        }
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
                     }
                 }
             }
