ea3fb37502805e08712cb3457651f1f323e8573a
Drink7 <johnnyliu505@gmail.com>
2023-09-25 20:51:58 +0800

    Time: 20 ms (51.6%), Space: 24.7 MB (6.03%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,82 +11,48 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        /*
-        // merge two list enhanced
-        // TC: O(nk), n is lists number, k is average list count
-        // SC: O(nk)
-        if (lists.size() == 0) {
-            return nullptr;
-        }
-
-        int n = lists.size();
-        for (int i = 1; i < n; i++) {
-            lists[0] = mergeTwoLists(lists[0], lists[i]);
-        }
-        return lists[0];
-        */
-
-        // Divide and conquer and merge
-        // TC: O(nk)
+        // TC: O(nlogn)
         // SC: O(1)
-        if (lists.size() == 0) {
+        return mergeSort(lists, 0, lists.size() - 1);
+    }
+
+    ListNode* mergeSort(vector<ListNode*>& lists, int left, int right) {
+        if (left > right) {
             return nullptr;
-        }
-
-        int n = lists.size();
-        while (n > 1) {
-            int group = (n + 1) / 2;
-            for (int i = 0; i < group; i++) {
-                lists[i] = mergeTwoLists(lists[i * 2], (i * 2 + 1) < n ? lists[(i * 2) + 1] : nullptr);
-            }
-            n = group;
-        }
-        return lists[0];
-    }
-    /*
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // merge two list, recursive
-        if (list1 == nullptr) {
-            return list2;
-        }
-
-        if (list2 == nullptr) {
-            return list1;
-        }
-
-        if (list1->val > list2->val) {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+        } else if (left == right) {
+            return lists[left];
         } else {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+            int mid = left + (right - left) / 2;
+            ListNode* leftNode = mergeSort(lists, left, mid);
+            ListNode* rightNode = mergeSort(lists, mid + 1, right);
+            return merge(leftNode, rightNode);
         }
     }
-    */
 
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // merge two list, iterative
+    ListNode* merge(ListNode* left, ListNode* right) {
         ListNode* dummy = new ListNode();
         ListNode* cur = dummy;
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val > list2->val) {
-                cur->next = list2;
-                list2 = list2->next;
+        while (left && right) {
+            if (left->val > right->val) {
+                cur->next = right;
+                right = right->next;
             } else {
-                cur->next = list1;
-                list1 = list1->next;
+                cur->next = left;
+                left = left->next;
             }
             cur = cur->next;
         }
 
-        if (list1 == nullptr) {
-            cur->next = list2;
+        if (left) {
+            cur->next = left;
         }
 
-        if (list2 == nullptr) {
-            cur->next = list1;
+        if (right) {
+            cur->next = right;
         }
 
-        return dummy->next;
+        cur = dummy->next;
+        delete(dummy);
+        return cur;
     }
 };
