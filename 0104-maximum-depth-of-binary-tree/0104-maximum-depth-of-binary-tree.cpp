c8d6c179c15d67d9964b407cd242f527cab59368
Drink7 <johnnyliu505@gmail.com>
2023-09-25 21:09:29 +0800

    Time: 10 ms (19.41%), Space: 19.3 MB (8.53%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,31 +12,29 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        // BFS
+        // use BFS
         // TC: O(n)
         // SC: O(n)
         if (root == nullptr) {
             return 0;
         }
+        int result = 0;
         queue<TreeNode*> q;
-        int maxDepth = 0;
         q.push(root);
         while (!q.empty()) {
             int qSize = q.size();
             for (int i = 0; i < qSize; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-
                 if (node->left) {
                     q.push(node->left);
                 }
-
                 if (node->right) {
                     q.push(node->right);
                 }
             }
-            maxDepth++;
+            result++;
         }
-        return maxDepth;
+        return result;
     }
 };
