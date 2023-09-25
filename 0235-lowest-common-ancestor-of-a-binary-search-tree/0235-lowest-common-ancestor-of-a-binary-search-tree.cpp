b3c5b5ccf602d92621cd0b425fbb3b5569080549
Drink7 <johnnyliu505@gmail.com>
2023-09-25 22:17:02 +0800

    Time: 28 ms (22.81%), Space: 23.5 MB (17.06%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,36 +11,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        /*
-        // check root->val (Recursive)
         // TC: O(logn)
-        // SC: O(logn) -> recursion stack
-        if (root == nullptr) {
-            return root;
-        }
-
-        if (p->val > root->val && q->val > root->val) {
-            return lowestCommonAncestor(root->right, p, q);
-        } else if (p->val < root->val && q->val < root->val){
+        // SC: O(1)
+        if (root->val > p->val && root->val > q->val) {
             return lowestCommonAncestor(root->left, p, q);
+        } else if (root->val < p->val && root->val < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
         } else {
             return root;
         }
-        */
-
-        // check root->val (Iterative)
-        // TC: O(logn)
-        // SC: O(1) -> recursion stack
-        TreeNode* cur = root;
-        while (cur != nullptr) {
-            if (p->val > cur->val && q->val > cur->val) {
-                cur = cur->right;
-            } else if (p->val < cur->val && q->val < cur->val) {
-                cur = cur->left;
-            } else {
-                break;
-            }
-        }
-        return cur;
     }
 };
