8bce1f4648830aa5cca34e4d42d50c89b2bf3805
Drink7 <johnnyliu505@gmail.com>
2023-09-25 21:14:03 +0800

    Time: 2 ms (43%), Space: 10.3 MB (15.74%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,19 +12,14 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // use postorder and see if the two tree the same
-        // TC: O(logn)
-        // SC: O(1)
+        // TC: O(n)
+        // SC: O(n)
         if (p == nullptr && q == nullptr) {
             return true;
         }
 
-        if (p != nullptr && q != nullptr) {
-            if (p->val == q->val) {
-                return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-            } else {
-                return false;
-            }
+        if (p != nullptr && q != nullptr && p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         } else {
             return false;
         }
