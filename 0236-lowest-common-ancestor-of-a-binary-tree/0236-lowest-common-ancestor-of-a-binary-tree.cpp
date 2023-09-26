67eda91397d2c5fc1e5abe7715ac3d0fc1510d38
Drink7 <johnnyliu505@gmail.com>
2023-09-26 21:58:17 +0800

    Time: 13 ms (61.7%), Space: 14.6 MB (21.93%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,18 +10,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // Use unordered_set to store ancestor nodes of p
-        // then traverse q, if root->left or root->right not existed in set, answer!
-        // else, keep tracking
-        // TC: O(n)
-        // SC: O(n)
-        if (root == nullptr || p == root || q == root) {
+        // TC: O(logn)
+        // SC: O(logn)
+        if (root == nullptr || root == p || root == q) {
             return root;
         }
 
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
         if (left && right) {
             return root;
         } else {
