9f2b4cb3295c89922300886764c0260f101e8c43
Drink7 <johnnyliu505@gmail.com>
2023-09-27 00:15:00 +0800

    Time: 14 ms (54.04%), Space: 24.3 MB (35.78%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -16,18 +16,24 @@ public:
         // check the k-th element
         // TC: O(n)
         // SC: O(n)
-        traverseBST(root);
-        return bstList[k - 1];
+        traverseBST(root, k);
+        return result;
     }
 
-    void traverseBST(TreeNode* root) {
+    void traverseBST(TreeNode* root, int k) {
         if (root == nullptr) {
             return;
         }
-        traverseBST(root->left);
-        bstList.push_back(root->val);
-        traverseBST(root->right);
+        traverseBST(root->left, k);
+        count++;
+        if (count == k) {
+            result = root->val;
+            return;
+        }
+        traverseBST(root->right, k);
     }
 private:
-    vector<int> bstList;
+    //vector<int> bstList;
+    int count = 0;
+    int result = -1;
 };
