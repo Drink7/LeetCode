e51ea24726f6159efbcf6cf8bfdf7ef40a2b9719
Drink7 <johnnyliu505@gmail.com>
2023-09-29 00:55:49 +0800

    Time: 2 ms (47.8%), Space: 6.7 MB (6.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        /*
-        // recursive
+        // dp
         // TC: O(n)
         // SC: O(n)
-        dp.resize(45 + 1);
+        vector<int> dp(n + 1);
         dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        return climbingHelper(n);
-        */
-
-        // Iterative
-        // TC: O(n)
-        // SC: O(n)
-        dp.resize(45 + 1);
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
-
-    int climbingHelper(int n) {
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-        dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
-        return dp[n];
-    }
-private:
-    vector<int> dp;
 };
