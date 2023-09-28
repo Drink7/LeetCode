9528dc6773819b6c59be2c801ccef02ba07b13d7
Drink7 <johnnyliu505@gmail.com>
2023-09-29 00:39:04 +0800

    Time: 29 ms (58.42%), Space: 12.5 MB (64.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,69 +1,34 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        // use union find
-        // traverse each grid, check the top and left side
-        // if top and left side is 1 too, union them
-        // TC: O(m * n)
+        // DFS
+        // TC: O(m*n)
         // SC: O(1)
         int m = grid.size();
         int n = grid[0].size();
-        parent.resize(m * n);
-        rank.resize(m * n);
-
-        // init
-        for (int i = 0; i < m * n; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        }
-
+        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    lands++;
-
-                    // union
-                    if (i - 1 >= 0 && grid[i - 1][j] == '1') {
-                        unionHelper(i * n + j, (i - 1) * n + j);
-                    }
-
-                    if (j - 1 >= 0 && grid[i][j - 1] == '1') {
-                        unionHelper(i * n + j, i * n + j - 1);
-                    }
+                    dfs(grid, i, j);
+                    count++;
                 }
             }
         }
-        return lands;
+        return count;
     }
-
-
-    void unionHelper(int x, int y) {
-        int rootX = findHelper(x);
-        int rootY = findHelper(y);
-        if (rootX == rootY) {
-            return;
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || row >= grid.size() ||
+            col < 0 || col >= grid[0].size() ||
+            grid[row][col] != '1') {
+                return;
+            }
+        
+        grid[row][col] = '0';
+        int dRow[4] = {-1, 0, 1, 0};
+        int dCol[4] = {0, -1, 0, 1};
+        for (int i = 0; i < 4; i++) {
+            dfs(grid, row + dRow[i], col + dCol[i]);
         }
-
-        if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
-        } else if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else {
-            parent[rootY] = rootX;
-            rank[rootX]++;
-        }
-        lands--;
     }
-
-    int findHelper(int x) {
-        if (parent[x] != x) {
-            parent[x] = findHelper(parent[x]);
-        }
-        return parent[x];
-    }
-
-private:
-    vector<int> parent;
-    vector<int> rank;
-    int lands = 0;
 };
