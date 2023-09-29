45156c53e8ac4b2f543bdb9b3baa3bfc0ebefd74
Drink7 <johnnyliu505@gmail.com>
2023-09-29 23:01:20 +0800

    Time: 101 ms (42.12%), Space: 40.5 MB (60.98%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Dijkstra's, minHeap
-        // TC: O(E logV)
-        // SC: O(E + V)
-        // store (next node, weight)
-        vector<vector<pair<int, int>>> graph(n + 1, vector<pair<int, int>>());
-        vector<int> distance(n + 1, INT_MAX);
-
-        // (weight, next node)
+        // dijkstra + minHeap to find the max time
+        // TC: O(E)
+        // SC: O(E)
+        unordered_map<int, vector<pair<int, int>>> graph;
+        vector<int> delayTimes(n + 1, INT_MAX);
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-        // build adjency
-        for (auto const& edge : times) {
-            graph[edge[0]].push_back({edge[1], edge[2]});
+        // build list
+        for (auto const& time : times) {
+            int src = time[0];
+            int dst = time[1];
+            int delay = time[2];
+            graph[src].push_back({dst, delay});
         }
 
-        // start from k
         pq.push({0, k});
-        distance[k] = 0;
+        delayTimes[k] = 0;
 
-        while (pq.size() > 0) {
+        while (!pq.empty()) {
             pair<int, int> p = pq.top();
             pq.pop();
             int curWeight = p.first;
             int curNode = p.second;
             for (int i = 0; i < graph[curNode].size(); i++) {
-                int nextNode = graph[curNode][i].first;
                 int nextWeight = graph[curNode][i].second;
-                if (curWeight + nextWeight < distance[nextNode]) {
-                    distance[nextNode] = curWeight + nextWeight;
-                    pq.push({distance[nextNode], nextNode});
+                int nextNode = graph[curNode][i].first;
+                
+                if (nextWeight + curWeight< delayTimes[nextNode]) {
+                    delayTimes[nextNode] = nextWeight + curWeight;
+                    pq.push({delayTimes[nextNode], nextNode});
                 }
             }
         }
-
+        
+        // find maximum delay
         int result = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if (distance[i] > result) {
-                result = distance[i];
+        for (int i = 1; i <= n; i++) {
+            if (delayTimes[i] > result) {
+                result = delayTimes[i] ;
             }
         }
         return result == INT_MAX ? -1 : result;
