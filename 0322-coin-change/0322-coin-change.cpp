7128bcef428f411f814c5ebcbb1d0f62035e3756
Drink7 <johnnyliu505@gmail.com>
2023-10-02 00:28:24 +0800

    Time: 46 ms (94.02%), Space: 14.9 MB (57.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // dynamic programming
-        // TC: O(mn), m is coins count, n is amount number
+        // TC: O(m * n), m is coins count, n is amount number
         // SC: O(n)
-        vector<int> dp(amount + 1, 10001);
+        vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
         for (int i = 0; i < coins.size(); i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j - coins[i] >= 0) {
-                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+            int coin = coins[i];
+            for (int j = 1; j <= amount; j++) {
+                if (j - coin >= 0) {
+                    dp[j] = min(dp[j - coin] + 1, dp[j]);
                 }
             }
         }
-        return (dp[amount] == 10001) ? -1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
