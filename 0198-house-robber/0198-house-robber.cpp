9467b6ecfdb71eb1b3b626990db3fb629d0cd867
Drink7 <johnnyliu505@gmail.com>
2023-10-01 12:54:20 +0800

    Time: 4 ms (23.12%), Space: 8.3 MB (6.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         // 1d dp
-        // check if nums[i] is robbed, how much we can get if we must rob nums[i] houses
-        // then get the max val of the last element in dp array
+        // compare current + dp[current - 2] and dp[current - 1]
+        // go to nums[i] does not mean we need to rob the house
         // TC: O(n)
         // SC: O(n)
-        int n = nums.size();
-        vector<int> dp(n);
-        dp[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            if (i == 1) {
-                dp[i] = max(dp[i - 1], nums[i]);
-            } else {
-                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
-            }
+        if (nums.size() == 1) {
+            return nums[0];
         }
-        return dp.back();
+
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[n - 1];
     }
 };
