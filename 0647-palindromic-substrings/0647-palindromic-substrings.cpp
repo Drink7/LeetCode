ef2115f6832e373884d60c146e9ad6ad88a9533e
Drink7 <johnnyliu505@gmail.com>
2023-10-01 23:51:26 +0800

    Time: 4 ms (77.66%), Space: 6.5 MB (74.42%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        // similar to LC. 5, but we count here
+        // similar to 5
+        // but we count the palindrome here
         // TC: O(n^2)
         // SC: O(1)
-        int result = 0;
+        int count = 0;
         for (int i = 0; i < s.size(); i++) {
             // odd
             int left = i;
             int right = i;
             while (left >= 0 && right < s.size() && s[left] == s[right]) {
-                result++;
+                count++;
                 left--;
                 right++;
             }
 
+            //even
             left = i;
             right = i + 1;
             while (left >= 0 && right < s.size() && s[left] == s[right]) {
-                result++;
+                count++;
                 left--;
                 right++;
             }
         }
-        return result;
+        return count;
     }
 };
