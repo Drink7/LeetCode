7f335296539cc940ebdb40d6ce827bc4fe27831f
Drink7 <johnnyliu505@gmail.com>
2023-10-01 11:50:18 +0800

    Time: 861 ms (17.97%), Space: 8.3 MB (21.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,43 +2,37 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         // DFS
-        // TC: O(m * n)
-        // SC: O(1)
-        int m = board.size();
-        int n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word[0]) {
-                    bool isExist = dfs(board, word, i, j, 0);
-                    if (isExist) {
-                        return true;
-                    }
+        // TC: O(m * n * 4 ^ L)
+        // SC: O(L)
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == word[0] && dfs(board, word, i, j, 0)) {
+                    return true;
                 }
             }
         }
         return false;
     }
 
-    bool dfs(vector<vector<char>>& board, string& word, int row, int col, int curIndex) {
-        if (curIndex == word.size()) {
+    bool dfs(vector<vector<char>>& board, string word, int row, int col, int start) {
+        if (start == word.size()) {
             return true;
         }
 
-        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size()
-            || board[row][col] != word[curIndex]) {
+        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() ||
+            board[row][col] == '#' || board[row][col] != word[start]) {
             return false;
         }
 
         char tmp = board[row][col];
         board[row][col] = '#';
-        bool isExisted = false;
+        int dRow[4] = {-1, 0, 1, 0};
+        int dCol[4] = {0, -1, 0, 1};
+        bool isExist = false;
         for (int i = 0; i < 4; i++) {
-            isExisted |= dfs(board, word, row + dRow[i], col + dCol[i], curIndex + 1);
+            isExist |= dfs(board, word, row + dRow[i], col + dCol[i], start + 1);
         }
         board[row][col] = tmp;
-        return isExisted;
+        return isExist;
     }
-private:
-    int dRow[4] = {0, 1, 0, -1};
-    int dCol[4] = {1, 0, -1, 0};
 };
