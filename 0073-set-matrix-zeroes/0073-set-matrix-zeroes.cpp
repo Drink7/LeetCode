b4f54af408880185d5c73418ae816a0da34ceb96
Drink7 <johnnyliu505@gmail.com>
2023-10-03 22:16:07 +0800

    Time: 11 ms (60.77%), Space: 13.7 MB (24.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        /*
         // use row / col sets to determine the ro / col need to be zero or not
         // TC: O(m * n)
         // SC: O(m + n)
@@ -29,6 +30,57 @@ public:
                 matrix[i][col] = 0;
             }
         }
+        */
 
+        // Best
+        // TC: O(m * n)
+        // SC: O(1)
+        bool isZeroRow = false;
+        bool isZeroCol = false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                isZeroRow = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                isZeroCol = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // handle
+        if (isZeroRow) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (isZeroCol) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
     }
 };
