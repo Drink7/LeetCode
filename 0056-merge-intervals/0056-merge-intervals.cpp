021b7b92a99047f9344cd3ff002129f04932a7a0
Drink7 <johnnyliu505@gmail.com>
2023-10-04 21:12:14 +0800

    Time: 162 ms (5.08%), Space: 48.4 MB (5%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // Sort the interval by their start first
-        // merge the interval, if can not merge, insert it to the result list
-        // TC: O(nlogn)
-        // SC: O(1)
-        vector<vector<int>> result;
-        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+        // sort interval start
+        // insert 1st interval
+        // check remaining interval, incoming interval start < last interval end, need to merge
+        // else insert
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
             return a[0] < b[0];
         });
-
-        // start to merge
-        // we first insert the first interval
+        vector<vector<int>> result;
         result.push_back(intervals[0]);
         for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] <= result.back()[1]) {
-                // merge interval
-                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            vector<int> interval = intervals[i];
+            // merge
+            if (result.back()[1] >= interval[0]) {
+                result.back()[1] = max(result.back()[1], interval[1]);
             } else {
-                result.push_back(intervals[i]);
+                result.push_back(interval);
             }
         }
         return result;
