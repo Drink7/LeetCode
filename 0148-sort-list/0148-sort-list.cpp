776155e5f4bcb28cca213373ed8fbc9ad04c1966
Drink7 <johnnyliu505@gmail.com>
2023-10-04 20:56:41 +0800

    Time: 104 ms (99.38%), Space: 53.6 MB (64.65%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
+        /*
         // store all nodes
         // might have duplicate node value
         // TC: O(nlogn)
@@ -35,5 +36,46 @@ public:
         cur = dummy->next;
         delete(dummy);
         return cur;
+        */
+        // merge sort
+        // TC:O(nlogn)
+        // SC: O(1)
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode* prev = nullptr;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            prev = slow;
+            slow = slow->next;
+        }
+
+        prev->next = nullptr;
+
+        // do left and right part merge sort
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(slow);
+        return mergeTwoList(left, right);
+    }
+
+    ListNode* mergeTwoList(ListNode* nodeA, ListNode* nodeB) {
+        if (nodeA == nullptr) {
+            return nodeB;
+        }
+
+        if (nodeB == nullptr) {
+            return nodeA;
+        }
+
+        if (nodeA->val > nodeB->val) {
+            nodeB->next = mergeTwoList(nodeA, nodeB->next);
+            return nodeB;
+        } else {
+            nodeA->next = mergeTwoList(nodeA->next, nodeB);
+            return nodeA;
+        }
     }
 };
