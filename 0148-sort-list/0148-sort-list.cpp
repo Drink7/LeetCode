a0a59bca0dda4a72790a32898869381c1a148a7e
Drink7 <johnnyliu505@gmail.com>
2023-10-13 00:13:44 +0800

    Time: 120 ms (91.18%), Space: 53.6 MB (66.05%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,71 +11,43 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        /*
-        // store all nodes
-        // might have duplicate node value
-        // TC: O(nlogn)
-        // SC: O(n)
-        map<int, vector<ListNode*>> nodeDict;
-        ListNode* dummy = new ListNode();
-        ListNode* cur = dummy;
-        // store
-        while (head != nullptr) {
-            nodeDict[head->val].push_back(head);
-            head = head->next;
-        }
-
-        // sort
-        for (auto const& p : nodeDict) {
-            for (auto const& node : p.second) {
-                cur->next = node;
-                cur = cur->next;
-            }
-        }
-        cur->next = nullptr;
-        cur = dummy->next;
-        delete(dummy);
-        return cur;
-        */
         // merge sort
-        // TC:O(nlogn)
+        // TC: O(nlogn)
         // SC: O(1)
         if (head == nullptr || head->next == nullptr) {
             return head;
         }
 
-        ListNode* prev = nullptr;
         ListNode* fast = head;
         ListNode* slow = head;
+        ListNode* prev = nullptr;
         while (fast != nullptr && fast->next != nullptr) {
             fast = fast->next->next;
             prev = slow;
             slow = slow->next;
         }
-
         prev->next = nullptr;
 
-        // do left and right part merge sort
         ListNode* left = sortList(head);
         ListNode* right = sortList(slow);
         return mergeTwoList(left, right);
     }
 
-    ListNode* mergeTwoList(ListNode* nodeA, ListNode* nodeB) {
-        if (nodeA == nullptr) {
-            return nodeB;
+    ListNode* mergeTwoList(ListNode* left, ListNode* right) {
+        if (left == nullptr) {
+            return right;
         }
 
-        if (nodeB == nullptr) {
-            return nodeA;
+        if (right == nullptr) {
+            return left;
         }
 
-        if (nodeA->val > nodeB->val) {
-            nodeB->next = mergeTwoList(nodeA, nodeB->next);
-            return nodeB;
+        if (left->val > right->val) {
+            right->next = mergeTwoList(left, right->next);
+            return right;
         } else {
-            nodeA->next = mergeTwoList(nodeA->next, nodeB);
-            return nodeA;
+            left->next = mergeTwoList(left->next, right);
+            return left;
         }
     }
 };
