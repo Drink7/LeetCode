c2ec55f19b5323f4164a5eb43679485baaee93b0
Drink7 <johnnyliu505@gmail.com>
2023-10-12 22:29:57 +0800

    Time: 0 ms (100%), Space: 9.2 MB (15.48%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        // Two pointer?
-        // Pointer point to first val appears
-        // Swap the pointer val when the next one is not equal to val
+        // two pointer
         // TC: O(n)
         // SC: O(1)
-        int pointer = 0;
+        int index = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {
-                swap(nums[pointer], nums[i]);
-                pointer++;
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return pointer;
+        return index;
     }
 };
