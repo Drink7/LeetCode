d1c96da06962c39c8e273d636b21a4add5ff96a7
Drink7 <johnnyliu505@gmail.com>
2023-10-15 14:44:53 +0800

    Time: 0 ms (100%), Space: 8.6 MB (37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // two pointer
+        // dutch national flags problem
         // TC: O(n)
         // SC: O(1)
         int left = 0;
         int right = nums.size() - 1;
-        int cur = left;
+        int cur = 0;
+        // 2 0 1
+        // 1 0 2
+        // 1 0 2
         while (cur <= right) {
             if (nums[cur] == 2) {
                 swap(nums[cur], nums[right]);
