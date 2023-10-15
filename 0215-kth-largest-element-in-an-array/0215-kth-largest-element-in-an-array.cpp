a732e304cadbe2f862fdba9166291ca69928c1bd
Drink7 <johnnyliu505@gmail.com>
2023-10-15 14:16:57 +0800

    Time: 102 ms (44.38%), Space: 58 MB (31.88%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,56 +1,17 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        /*
-        // MaxHeap Priority Queue
-        // TC: O(nlogn + k)
-        // SC: O(n)
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        for (auto const& num : nums) {
-            minHeap.push(num);
-            if (minHeap.size() > k) {
-                minHeap.pop();
+        // priority_queue heap
+        // min heap store k elements
+        // TC: O(nlogk)
+        // SC: O(k)
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+            if (pq.size() > k) {
+                pq.pop();
             }
         }
-
-        return minHeap.top();
-        */
-        // Quick Select
-        // find k largest, quick select n - k large
-        // TC: O(n)
-        // SC: O(1)
-        //return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
-
-        // STL nth_element used to find k-th smallest
-        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
-        return nums[k - 1];
-    }
-
-    int quickSelect(vector<int>& nums, int left, int right, int k) {
-        if (left == right) {
-            return nums[left];
-        }
-
-        int pivot = partition(nums, left, right);
-        if (pivot == k) {
-            return nums[k];
-        } else if (pivot > k) {
-            return quickSelect(nums, left, pivot - 1, k);
-        } else {
-            return quickSelect(nums, pivot + 1, right, k);
-        }
-    }
-
-    int partition(vector<int>& nums, int left, int right) {
-        int i = left;
-        int pivot = nums[right];
-        for (int j = left; j < right; j++) {
-            if (nums[j] <= pivot) {
-                swap(nums[i], nums[j]);
-                i++;
-            }
-        }
-        swap(nums[i], nums[right]);
-        return i;
+        return pq.top();
     }
 };
