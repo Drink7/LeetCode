0737b46a2988ac8418bde44c6bc5cb30da2d0d87
Drink7 <johnnyliu505@gmail.com>
2023-10-15 14:00:40 +0800

    Time: 208 ms (7.12%), Space: 84.3 MB (5.04%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,13 +11,13 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        // merge sort
         // TC: O(nlogn)
-        // SC: O(1)
+        // SC: O(n)
         if (head == nullptr || head->next == nullptr) {
             return head;
         }
 
+        // find mid
         ListNode* fast = head;
         ListNode* slow = head;
         ListNode* prev = nullptr;
@@ -30,24 +30,32 @@ public:
 
         ListNode* left = sortList(head);
         ListNode* right = sortList(slow);
-        return mergeTwoList(left, right);
+        return mergeList(left, right);
     }
 
-    ListNode* mergeTwoList(ListNode* left, ListNode* right) {
-        if (left == nullptr) {
-            return right;
+    ListNode* mergeList(ListNode* left, ListNode* right) {
+        ListNode* dummy = new ListNode();
+        ListNode* cur = dummy;
+        while (left && right) {
+            if (left->val > right->val) {
+                cur->next = right;
+                right = right->next;
+            } else {
+                cur->next = left;
+                left = left->next;
+            }
+            cur = cur->next;
         }
 
-        if (right == nullptr) {
-            return left;
+        if (left) {
+            cur->next = left;
         }
 
-        if (left->val > right->val) {
-            right->next = mergeTwoList(left, right->next);
-            return right;
-        } else {
-            left->next = mergeTwoList(left->next, right);
-            return left;
+        if (right) {
+            cur->next = right;
         }
+        cur = dummy->next;
+        delete(dummy);
+        return cur;
     }
 };
