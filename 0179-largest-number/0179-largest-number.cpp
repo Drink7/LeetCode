6ede25d9516d27ec4b7c5a9982405db2a290aeeb
Drink7 <johnnyliu505@gmail.com>
2023-10-15 14:07:26 +0800

    Time: 3 ms (94.73%), Space: 11.4 MB (75.15%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        // mod 10 to and sort
-        // if the moded numbr is the same, put the fewer digit first
-        // if digit the same, compare the value
+        // tricky sort (s1 + s2) > (s2 + s1)
         // TC: O(nlogn)
         // SC: O(1)
-        string result;
-
-        sort(nums.begin(), nums.end(), [](int a, int b) {
-           string strA = to_string(a);
-           string strB = to_string(b);
-           return strA + strB > strB + strA;
+        sort(nums.begin(), nums.end(), [](int& num1, int& num2){
+            string s1 = to_string(num1);
+            string s2 = to_string(num2);
+            return s1 + s2 > s2 + s1;
         });
 
-        // if nums is all zero, return directly
-        if (nums[0] == 0) {
-            return "0";
-        }
-    
-        // then append all these value
+        // concat
+        string result = "";
         for (int i = 0; i < nums.size(); i++) {
+            if (nums[0] == 0) {
+                return "0";
+            }
             result += to_string(nums[i]);
         }
-
         return result;
     }
 };
