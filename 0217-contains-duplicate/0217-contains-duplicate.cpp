bae1a7be7ac91a36ff2f9ce5de8196cd151535da
Drink7 <johnnyliu505@gmail.com>
2023-10-19 20:02:18 +0800

    Time: 88 ms (85.51%), Space: 57.7 MB (77.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        /*
         // hash set
         // TC: O(n)
         // SC: O(n)
-        unordered_set<int> dict;
-        for (auto const& num : nums) {
-            if (dict.count(num)) {
+        unordered_set<int> numSet;
+        for (int i = 0; i < nums.size(); i++) {
+            if (numSet.count(nums[i])) {
                 return true;
             }
-            dict.insert(num);
+            numSet.insert(nums[i]);
+        }
+        return false;
+        */
+        // sort
+        // TC: O(nlogn)
+        // SC: O(1)
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
         }
         return false;
     }
