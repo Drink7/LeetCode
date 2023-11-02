0869c5cd75fc81868dd55cce2f5730baed5423fe
Drink7 <johnnyliu505@gmail.com>
2023-11-02 21:57:34 +0800

    Time: 11 ms (46.29%), Space: 7.6 MB (60.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // TC: O(slogs + tlogt)
+        // sort nd compare
+        // TC: O(nlogn)
         // SC: O(1)
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
