09b88c5c6ed889a1e7dbf33646c7443a22a6d877
Drink7 <johnnyliu505@gmail.com>
2023-11-03 21:58:24 +0800

    Time: 11 ms (75.14%), Space: 14.1 MB (37.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,15 +4,18 @@ public:
         // heap -> priority_queue (minHeap)
         // map -> store element, freq
         // freq into heap, and keep the heap with k size
+        // TC: O(nlogn)
+        // SC: O(k)
         unordered_map<int, int> freqDict;
         for (auto const& num : nums) {
             freqDict[num]++;
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        auto compare = [](pair<int, int>& p1, pair<int, int>& p2) { return p1.second > p2.second; };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(compare)> pq(compare);
 
         for (auto const& p : freqDict) {
-            pq.push({p.second, p.first});
+            pq.push(p);
             if (pq.size() > k) {
                 pq.pop();
             }
@@ -20,7 +23,7 @@ public:
 
         vector<int> result;
         for (int i = 0; i < k; i++) {
-            result.push_back(pq.top().second);
+            result.push_back(pq.top().first);
             pq.pop();
         }
         return result;
