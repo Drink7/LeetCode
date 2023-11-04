08f7e5065fac9e2615d38e25e619f855dd6c767e
Drink7 <johnnyliu505@gmail.com>
2023-11-04 13:32:25 +0800

    Time: 4 ms (80.66%), Space: 7.5 MB (73.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -6,16 +6,17 @@ public:
         // SC: O(1)
         int left = 0;
         int right = s.size() - 1;
-        while (left <= right) {
-            while (left <= right && !isalpha(s[left]) && !isdigit(s[left])) {
+        while (left < right) {
+            // isalpha or isdigit
+            while (left < right && !isalpha(s[left]) && !isdigit(s[left])) {
                 left++;
             }
 
-            while (left <= right && !isalpha(s[right]) && !isdigit(s[right])) {
+            while (left < right && !isalpha(s[right]) && !isdigit(s[right])) {
                 right--;
             }
-            
-            if (left <= right && tolower(s[left]) != tolower(s[right])) {
+
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
             left++;
