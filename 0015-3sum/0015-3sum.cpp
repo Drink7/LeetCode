a8e61bb49e2e84b34a3fc3b54a3fbcb057d8c602
Drink7 <johnnyliu505@gmail.com>
2023-11-04 15:01:01 +0800

    Time: 164 ms (45.51%), Space: 24.6 MB (64.48%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // two pointer
-        // TC: O(n^2)
+        // first sort
+        // duplicate prevention
+        // TC: O(nlogn + n^2)
         // SC: O(1)
-
-        // sort first
         sort(nums.begin(), nums.end());
-
         vector<vector<int>> result;
         int n = nums.size();
         for (int i = 0; i < n; i++) {
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                int target = 0 - nums[i];
+            // duplicate prevention
+            if (i == 0 || nums[i] != nums[i - 1]) {
                 int left = i + 1;
                 int right = n - 1;
                 while (left < right) {
-                    int sum = nums[left] + nums[right];
-                    if (sum == target) {
+                    if (nums[i] + nums[left] + nums[right] == 0) {
                         result.push_back({nums[i], nums[left], nums[right]});
-                        
+
+                        // duplicate prevention
                         left++;
                         right--;
-                        
-                        // skip left
-                        while (left < right && nums[left - 1] == nums[left]) {
+                        while (left < right && nums[left] == nums[left - 1]) {
                             left++;
                         }
 
-                        // skip right
-                        while (left < right && nums[right + 1] == nums[right]) {
+                        while (left < right && nums[right] == nums[right + 1]) {
                             right--;
                         }
-                    } else if (sum > target) {
+                    } else if (nums[i] + nums[left] + nums[right] > 0) {
                         right--;
                     } else {
                         left++;
