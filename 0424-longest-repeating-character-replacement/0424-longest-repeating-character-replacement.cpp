f1c76bf75930d6af06b45b9bb1cd738a24961835
Drink7 <johnnyliu505@gmail.com>
2023-11-09 21:18:31 +0800

    Time: 14 ms (28.25%), Space: 7.2 MB (93.75%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // vector to store letter count
-        // string.size() > max letter cnt + k
-        // need to move left
-        // or continue moving right
+        // sliding window + count max
         // TC: O(n)
-        // SC: O(n)
-        vector<int> letterArr(26, 0);
+        // SC: O(1)
+        vector<int> letters(26, 0);
         int left = 0;
         int right = 0;
-        int n = s.size();
+        int n = s.length();
         int result = 0;
         while (right < n) {
             char rightChar = s[right];
-            letterArr[rightChar - 'A']++;
-
-            // left move condition
-            while (left < right && right - left + 1 > k + findMax(letterArr)) {
-                letterArr[s[left] - 'A']--;
+            letters[rightChar - 'A']++;
+            while ((right - left + 1) - findMax(letters) > k) {
+                letters[s[left] - 'A']--;
                 left++;
             }
-            result = max(result, right - left + 1);
+            result = max(result, (right - left + 1));
             right++;
         }
         return result;
     }
 
-    int findMax(vector<int>& nums) {
+    int findMax(vector<int>& arr) {
         int result = 0;
-        for (auto const& num : nums) {
+        for (auto const& num : arr) {
             if (num > result) {
                 result = num;
             }
