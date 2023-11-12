f9d28f2aa305967af3af66967acb4e28c4146e32
Drink7 <johnnyliu505@gmail.com>
2023-11-12 22:06:25 +0800

    Time: 46 ms (53.48%), Space: 55.8 MB (38.78%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // Similar to 424.  Longest Repeating Character Replacement
+        // right - left + 1 - oneCnt <= k
         // TC: O(n)
         // SC: O(1)
-        vector<int> count(2, 0);
         int n = nums.size();
         int left = 0;
         int right = 0;
+        int oneCnt = 0;
         int result = 0;
         while (right < n) {
-            count[nums[right]]++;
+            if (nums[right] == 1) {
+                oneCnt++;
+            }
 
-            // check if we can flip k or not
-            // if we can not, move left pointer
-            while (right - left + 1 > count[1] + k) {
-                count[nums[left]]--;
+            while ((right - left + 1) - oneCnt > k) {
+                if (nums[left] == 1) {
+                    oneCnt--;
+                }
                 left++;
             }
-            result = max(result, right - left + 1);
+            result = max(result, (right - left + 1));
             right++;
         }
         return result;
