c0f06256464f47037d28fd32aaed09d83b885752
Drink7 <johnnyliu505@gmail.com>
2023-11-13 22:49:07 +0800

    Time: 4 ms (48.13%), Space: 10.4 MB (9.84%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        // TC: O(m + n), m is str1 length, n is str2 length
-        // SC: O(m + n)
-        if (str1.size() < str2.size()) {
-            return gcdOfStrings(str2, str1);
+        // TC: O(m + n)
+        // SC: O(1)
+
+        if (str1 + str2 != str2 + str1) {
+            return "";
         }
 
         if (str1 == str2) {
             return str1;
+        } else if (str1.size() > str2.size()) {
+            return gcdOfStrings(str2, str1.substr(str2.size()));
+        } else {
+            return gcdOfStrings(str1, str2.substr(str1.size()));
         }
-
-        if (str1.find(str2) != 0) {
-            return "";
-        }
-    
-        str1 = str1.substr(str2.size());
-        return gcdOfStrings(str1, str2);
     }
 };
