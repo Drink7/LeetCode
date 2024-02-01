f3130d0af1a2f9643d8f4a6f75c7325bfdaa83e4
Drink7 <johnnyliu505@gmail.com>
2024-02-01 21:08:53 +0800

    Time: 4 ms (38.15%), Space: 7.1 MB (6.91%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -5,18 +5,11 @@ public:
         // TC: O(1)
         // SC: O(1)
         int result = 0;
-        int carry = 0;
         for (int i = 0; i < 32; i++) {
-            result = result | carry;
-            if (n & 1 == 1) {
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-            n = n >> 1;
             result = result << 1;
+            result = result | (n & 1);
+            n = n >> 1;
         }
-        result = result | carry;
         return result;
     }
 };
