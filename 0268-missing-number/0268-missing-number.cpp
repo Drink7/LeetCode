20c6c09cc4815574672e9d6ae34ef63f41cffd5e
Drink7 <johnnyliu505@gmail.com>
2024-02-01 21:17:28 +0800

    Time: 7 ms (98.05%), Space: 20.2 MB (22.06%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // XOR two num
+        // XOR
         // TC: O(n)
         // SC: O(1)
         int result = nums.size();
         for (int i = 0; i < nums.size(); i++) {
-            result = result ^ i ^ nums[i];
+            result = i ^ result ^ nums[i];
         }
         return result;
     }
