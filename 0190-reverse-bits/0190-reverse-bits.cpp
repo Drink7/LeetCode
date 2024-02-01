b2c24b31fbad132afa1cb5b0d6fbd3db37b58ee0
Drink7 <johnnyliu505@gmail.com>
2024-02-01 21:04:45 +0800

    Time: 2 ms (51.25%), Space: 7.1 MB (15.76%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        // brute force
+        // traverse 32 times
         // TC: O(1)
         // SC: O(1)
-        uint32_t result = 0;
-
+        int result = 0;
+        int carry = 0;
         for (int i = 0; i < 32; i++) {
-            result = result << 1;
-            result = result | (n & 1);
+            result = result | carry;
+            if (n & 1 == 1) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
             n = n >> 1;
+            result = result << 1;
         }
-
+        result = result | carry;
         return result;
     }
 };
