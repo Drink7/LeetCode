ff00583eef6884f326e6c4d740667b6a96230c79
Drink7 <johnnyliu505@gmail.com>
2024-02-02 22:29:03 +0800

    Time: 4 ms (40.1%), Space: 7 MB (42.98%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
     int integerReplacement(int n) {
-        // Recursion
         // TC: O(logn)
-        // SC: O(1)
-        //return replaceHelper(n);
-
-        // Bit operation
-        // if n end up with 01, use n - 1
-        // if n end up with 11, use n + 1
-        long bigN = n;
-        int count = 0;
-        while (bigN != 1) {
-            if ((bigN & 1) == 0) {
-                bigN = bigN >> 1;
-            } else {
-                if ((bigN & 2) == 0 || bigN == 3) {
-                    bigN = bigN - 1;
-                } else {
-                    bigN = bigN + 1;
-                }
-            }
-            count++;
-        }
-        return count;
-    }
-
-    int replaceHelper(long n) {
+        // SC: O(logn)
+        // Recursion
         if (n == 1) {
             return 0;
         }
-
         if (n % 2 == 0) {
-            return replaceHelper(n / 2) + 1;
+            return 1 + integerReplacement(n / 2);
         } else {
-            return min(replaceHelper(n + 1) + 1, replaceHelper(n - 1) + 1);
+            long t = n;
+            return 1 + 1 + min(integerReplacement((t + 1) / 2), integerReplacement((t - 1) / 2));
         }
     }
 };
