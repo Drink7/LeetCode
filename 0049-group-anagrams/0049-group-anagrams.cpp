b725fa6bcd3deee2a1638b2b1f240aa319cba790
Drink7 <johnnyliu505@gmail.com>
2024-08-25 00:48:54 +0800

    Time: 23 ms (83.45%), Space: 24 MB (74.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // sort each str in strs
+        // store vector in map
+        // return result in vector<vector<string>> way
         // TC: O(m * nlogn)
-        // SC: (m * n)
-        unordered_map<string, vector<string>> groupDict;
+        // SC: O(m * n)
+        unordered_map<string, vector<string>> groupMap;
         vector<vector<string>> result;
         for (auto const& str : strs) {
-            string tmp = str;
-            sort(tmp.begin(), tmp.end());
-            groupDict[tmp].push_back(str);
+            string sorted = str;
+            sort(sorted.begin(), sorted.end());
+            groupMap[sorted].push_back(str);
         }
 
-        // add to result
-        for (auto const& p : groupDict) {
+        // return result
+        for (auto const& p : groupMap) {
             result.push_back(p.second);
         }
         return result;
