9c175d7392afc31b9aafd1139f22cab08543f575
Drink7 <johnnyliu505@gmail.com>
2024-08-26 00:04:22 +0800

    Time: 6 ms (95.29%), Space: 17.5 MB (47.4%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // heap -> priority_queue (minHeap)
-        // map -> store element, freq
-        // freq into heap, and keep the heap with k size
+        // store the number to map
+        // store freq number in min heap
+        // pop all pair in heap for result
+        // return result
         // TC: O(nlogn)
-        // SC: O(k)
-        unordered_map<int, int> freqDict;
+        // SC: O(n)
+        unordered_map<int, int> dict;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> result;
         for (auto const& num : nums) {
-            freqDict[num]++;
+            dict[num]++;
         }
 
-        auto compare = [](pair<int, int>& p1, pair<int, int>& p2) { return p1.second > p2.second; };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(compare)> pq(compare);
+        // store in min heap
+        for (auto const& p : dict) {
+            pq.push({p.second, p.first});
 
-        for (auto const& p : freqDict) {
-            pq.push(p);
             if (pq.size() > k) {
                 pq.pop();
             }
         }
 
-        vector<int> result;
         for (int i = 0; i < k; i++) {
-            result.push_back(pq.top().first);
+            result.push_back(pq.top().second);
             pq.pop();
         }
         return result;
