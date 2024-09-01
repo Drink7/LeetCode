8c1478dc025b65b970eb71cea62a472fa4507911
Drink7 <johnnyliu505@gmail.com>
2024-09-02 00:40:12 +0800

    Time: 22 ms (31.52%), Space: 14.6 MB (26.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // sliding window + hashset
+        // substring, longest -> sliding window
+        // sliding window
         // TC: O(n)
         // SC: O(n)
-        unordered_set<char> dict;
         int left = 0;
         int right = 0;
-        int n = s.size();
         int result = 0;
-        while (right < n) {
-            if (dict.count(s[right]) == 0) {
-                dict.insert(s[right]);
-                result = max(result, right - left + 1);
-                right++;
-            } else {
-                while (dict.count(s[right])) {
-                    dict.erase(s[left]);
-                    left++;
-                }
+        unordered_set<char> dict;
+        while (right < s.size()) {
+            char c = s[right];
+            while (dict.count(c) > 0) {
+                dict.erase(s[left]);
+                left++;
             }
+            dict.insert(c);
+            result = max(result, right - left + 1);
+            right++;
         }
         return result;
     }
