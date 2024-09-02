577fe2ff557511fce304d86933f0db80e0c9ff0b
Drink7 <johnnyliu505@gmail.com>
2024-09-03 00:42:22 +0800

    Time: 6 ms (78.97%), Space: 8.5 MB (53.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // sliding window + count max
-        // TC: O(n)
-        // SC: O(1)
-        vector<int> letters(26, 0);
-        int left = 0;
+        // longest, substring -> sliding window
+        // check max freq
+        // right - left + 1 - max freq <= k
+        // TC: O(26 * n)
+        // SC: O(n)
+        vector<int> charDict(26, 0);
         int right = 0;
-        int n = s.length();
+        int left = 0;
         int result = 0;
-        while (right < n) {
-            char rightChar = s[right];
-            letters[rightChar - 'A']++;
-            while ((right - left + 1) - findMax(letters) > k) {
-                letters[s[left] - 'A']--;
+        while (right < s.size()) {
+            charDict[s[right] - 'A']++;
+            while (right - left + 1 - findMax(charDict) > k) {
+                charDict[s[left] - 'A']--;
                 left++;
             }
-            result = max(result, (right - left + 1));
+            result = max(result, right - left + 1);
             right++;
         }
         return result;
     }
 
-    int findMax(vector<int>& arr) {
+    int findMax(vector<int>& nums) {
         int result = 0;
-        for (auto const& num : arr) {
+        for (auto const& num : nums) {
             if (num > result) {
                 result = num;
             }
