9af6d936c53065d2e87e4f2855c8ebd8398f844b
Drink7 <johnnyliu505@gmail.com>
2024-09-08 15:15:18 +0800

    Time: 0 ms (100%), Space: 13.8 MB (90.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // binary search
+        // logn -> binary search
         // TC: O(logn)
         // SC: O(1)
-        int left = 0;
-        int right = nums.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        //  target = 4
+        // 1 2 3 4 5, mid < target
+        // 5 1 2 3 4, mid < target
+        // 4 5 1 2 3, mid < target
+        // 3 4 5 1 2, mid > target
+        // 2 3 4 5 1
+        // mid < target
+        // -> mid > low, find right
+        // -> else, find left
+        // mid > target
+        // -> mid <= high, find right
+        // -> else, find left
+
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else {
-                if (nums[mid] <= nums[right]) {
-                    // right part still sorted
-                    if (target > nums[mid] && target <= nums[right]) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
+            }
+            
+            // check if sorted in the later half
+            // right half sorted
+            if (nums[mid] < nums[high]) {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
                 } else {
-                    if (target >= nums[left] && target < nums[mid]) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }
+                    high = mid - 1;
+                }
+            } else {
+                // left half sorted
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
             }
         }
