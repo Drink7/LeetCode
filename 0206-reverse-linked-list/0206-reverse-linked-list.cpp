2169c0718f688a80e7e236b7b6730710d9c3f4ca
Drink7 <johnnyliu505@gmail.com>
2024-09-08 15:56:20 +0800

    Time: 8 ms (28.47%), Space: 11.8 MB (96.89%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,7 +11,8 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        // recursive way
+        /*
+        // Iterative
         // TC: O(n)
         // SC: O(1)
         ListNode* prev = nullptr;
@@ -22,5 +23,31 @@ public:
             head = next;
         }
         return prev;
+        */
+        // Recursive
+        // TC: O(n)
+        // SC: O(n)
+        //return reverseHelper(nullptr, head);
+
+        // Recursive II
+        // TC: O(n)
+        // SC: O(n)
+        if (head == nullptr || head->next == nullptr) {
+                return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+
+    ListNode* reverseHelper(ListNode* prev, ListNode* head) {
+        if (head == nullptr) {
+            return prev;
+        }
+
+        ListNode* next = head->next;
+        head->next = prev;
+        return reverseHelper(head, next);
     }
 };
