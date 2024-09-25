eca8462a569795d4f89f0d1334011a0847556030
Drink7 <johnnyliu505@gmail.com>
2024-09-26 00:02:08 +0800

    Time: 20 ms (79.67%), Space: 22.1 MB (19.51%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,31 +11,26 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // TC: O(logn)
-        // SC: O(1)
-        /*
+        // BST -> in-order
+        // TC: O(n)
+        // SC: O(n)
+        if (p->val > q->val) {
+            return lowestCommonAncestor(root, q, p);
+        }
         if (root == nullptr) {
             return root;
         }
 
-        if (root->val > p->val && root->val > q->val) {
-            return lowestCommonAncestor(root->left, p, q);
-        } else if (root->val < p->val && root->val < q->val) {
-            return lowestCommonAncestor(root->right, p, q);
-        } else {
+        if ((root->val > p->val && root->val < q->val) ||
+            root->val == p->val ||
+            root->val == q->val) {
             return root;
-        }
-        */
-
-        while (root) {
-            if (root->val > p->val && root->val > q->val) {
-                root = root->left;
-            } else if (root->val < p->val && root->val < q->val) {
-                root = root->right;
+        } else {
+            if (root->val > q->val) {
+                return lowestCommonAncestor(root->left, p, q);
             } else {
-                break;
+                return lowestCommonAncestor(root->right, p, q);
             }
         }
-        return root;
     }
 };
