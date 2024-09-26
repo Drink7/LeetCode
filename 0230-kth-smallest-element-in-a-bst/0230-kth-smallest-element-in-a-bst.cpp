dc861bde15f2ceb5e99fb518e8a8096e3e684942
Drink7 <johnnyliu505@gmail.com>
2024-09-26 23:44:06 +0800

    Time: 5 ms (95.55%), Space: 22.8 MB (72.37%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -12,28 +12,26 @@
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        // inorder traversal to list all nodes
-        // check the k-th element
-        // TC: O(n)
-        // SC: O(n)
-        traverseBST(root, k);
-        return result;
+        // TC: O(k)
+        // SC: O(k)
+        kthHelper(root, k);
+        return result->val;
     }
 
-    void traverseBST(TreeNode* root, int k) {
+    void kthHelper(TreeNode* root, int k) {
         if (root == nullptr) {
             return;
         }
-        traverseBST(root->left, k);
-        count++;
-        if (count == k) {
-            result = root->val;
+
+        kthHelper(root->left, k);
+        cnt++;
+        if (cnt == k) {
+            result = root;
             return;
         }
-        traverseBST(root->right, k);
+        kthHelper(root->right, k);
     }
 private:
-    //vector<int> bstList;
-    int count = 0;
-    int result = -1;
+    TreeNode* result;
+    int cnt = 0;
 };
