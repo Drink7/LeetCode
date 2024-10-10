9338d1fbcb07af70e1ca10a773dbb8157f74ca3f
Drink7 <johnnyliu505@gmail.com>
2024-10-10 23:54:39 +0800

    Time: 29 ms (53.69%), Space: 17.4 MB (42.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,57 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        /*
+        // DFS with 4 directions
+        // TC: O(m * n)
+        // SC: O(m * n)
+        int m = grid.size();
+        int n = grid[0].size();
+        int islands = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+        */
         // union find
-        // TC: O(n)
-        // SC: O(n)
+        // TC: O(m * n)
+        // SC: O(m * n)
         int m = grid.size();
         int n = grid[0].size();
         parent.resize(m * n);
         rank.resize(m * n);
+
         for (int i = 0; i < m * n; i++) {
             parent[i] = i;
             rank[i] = 1;
         }
 
+        int lands = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    island++;
+                    lands++;
+                    // check actual lands after union find
+                    // find up
                     if (i > 0 && grid[i - 1][j] == '1') {
-                        unionHelper((i - 1) * n + j, i * n + j);
+                        unionHelper(i * n + j, (i - 1) * n + j, lands);
                     }
-
+                    // find left
                     if (j > 0 && grid[i][j - 1] == '1') {
-                        unionHelper(i * n + j - 1, i * n + j);
+                        unionHelper(i * n + j, i * n + (j - 1), lands);
                     }
                 }
             }
         }
-        return island;
+        return lands;
     }
 
-    void unionHelper(int x, int y) {
+    void unionHelper(int x, int y, int& lands) {
         int rootX = findHelper(x);
         int rootY = findHelper(y);
         if (rootX == rootY) {
@@ -45,7 +66,7 @@ public:
             parent[rootY] = rootX;
             rank[rootX]++;
         }
-        island--;
+        lands--;
     }
 
     int findHelper(int x) {
@@ -55,8 +76,21 @@ public:
         return parent[x];
     }
 
+    /*
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0';
+        for (int i = 0; i < 4; i++) {
+            dfs(grid, row + dRow[i], col + dCol[i]);
+        }
+    }
+    */
+
 private:
+    //int dRow[4] = {-1, 0, 1, 0};
+    //int dCol[4] = {0, -1, 0, 1};
     vector<int> parent;
     vector<int> rank;
-    int island = 0;
 };
