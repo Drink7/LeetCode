4f36f8182d22f04963de29f49e066ba766a05b5f
Drink7 <johnnyliu505@gmail.com>
2025-07-06 16:25:17 +0800

    Time: 239 ms (69.42%), Space: 10.5 MB (75.7%) - LeetHub

---
+++ 0079-word-search/0079-word-search-07-06-2025-16-25-17.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        // dfs walk up down left right
+        // if exist,stop explore
+        // TC: O(m * n * 4^k), k is word size
+        // SC: O(k)
+        int m = board.size();
+        int n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    explorer(board, word, i, j, 0);
+                }
+
+                if (isExist) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void explorer(vector<vector<char>>& board, string& word, int row, int col, int start) {
+        if (row < 0 ||
+            col < 0 ||
+            row >= board.size() ||
+            col >= board[0].size() ||
+            board[row][col] == '-' ||
+            board[row][col] != word[start]) {
+            return;
+        }
+
+        if (start == word.size() - 1) {
+            isExist = true;
+            return;
+        }
+
+        char tmp = board[row][col];
+        board[row][col] = '-';
+
+        for (int i = 0; i < 4; i++) {
+            explorer(board, word, row + dx[i], col + dy[i], start + 1);
+        }
+
+        board[row][col] = tmp;
+    }
+private:
+    bool isExist = false;
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, -1, 0, 1};
+};
