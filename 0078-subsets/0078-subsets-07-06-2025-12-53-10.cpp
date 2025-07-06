e4dceada502326094ef1b260b23f69cf45989e8a
Drink7 <johnnyliu505@gmail.com>
2025-07-06 12:53:11 +0800

    Time: 0 ms (100%), Space: 7.3 MB (100%) - LeetHub

---
+++ 0078-subsets/0078-subsets-07-06-2025-12-53-10.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // backtracking
+        // TC: O(2^n)
+        // SC: O(2^n)
+        vector<vector<int>> result;
+        vector<int> subset;
+        subsetHelper(nums, result, subset, 0);
+        return result;
+    }
+
+    void subsetHelper(vector<int>& nums, vector<vector<int>>& result, vector<int> subset, int index) {
+        /*
+        if (index == nums.size()) {
+            result.push_back(subset);
+            return;
+        }
+
+        
+        // choose
+        subset.push_back(nums[index]);
+        subsetHelper(nums, result, subset, index + 1);
+
+        // not choose
+        subset.pop_back();
+        subsetHelper(nums, result, subset, index + 1);
+        */
+
+        result.push_back(subset);
+
+        for (int i = index; i < nums.size(); i++) {
+            // choose
+            subset.push_back(nums[i]);
+            subsetHelper(nums, result, subset, i + 1);
+            // not choose
+            subset.pop_back();
+        }
+    }
+};
