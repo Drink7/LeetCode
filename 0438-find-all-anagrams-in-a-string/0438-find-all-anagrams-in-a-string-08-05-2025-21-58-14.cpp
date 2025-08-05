0c172672900f1fd4e017f16ea74a056e569bbe1b
Drink7 <johnnyliu505@gmail.com>
2025-08-05 21:58:15 +0800

    Time: 3 ms (82.36%), Space: 11.5 MB (84.71%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string-08-05-2025-21-58-14.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        // p store hash_map<char, int>
+        // sliding window,
+        // anagramCnt = p.size()
+        // if map[right] > 0, anagramCnt--;
+        // map[right]--;
+        // if map[left] >= 0, anagramCnt++;
+        // map[left]++;
+        // TC: O(m + n)
+        // SC: O(n)
+        unordered_map<char, int> dict;
+        vector<int> result;
+        for (auto const& c : p) {
+            dict[c]++;
+        }
+
+        int anagramCnt = p.size();
+        int left = 0, right = 0;
+        while (right < s.size()) {
+            dict[s[right]]--;
+            if (dict[s[right]] >= 0) {
+                anagramCnt--;
+            }
+
+            if (right - left + 1 == p.size()) {
+                if (anagramCnt == 0) {
+                    result.push_back(left);
+                }
+
+                dict[s[left]]++;
+                if (dict[s[left]] > 0) {
+                    anagramCnt++;
+                }
+                left++;
+            }
+            right++;
+        }
+        return result;
+    }
+};
