f7d108acb45e2e3302073ce14a2f2c105b8cc8b9
Drink7 <johnnyliu505@gmail.com>
2025-08-13 00:02:04 +0800

    Time: 0 ms (100%), Space: 52.1 MB (98.98%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game-08-13-2025-00-02-03.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        /*
+        // dp[i] is true when dp[j] is true && j + nums[j] >= i
+        // j < i
+        // TC: O(n^2)
+        // SC: O(n)
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        if (nums[0] != 0 || nums.size() == 1) {
+            dp[0] = true;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && (j + nums[j] >= i)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n - 1];
+        */
+
+        // Greedy
+        // TC: O(n)
+        // SC: O(1)
+        int reachable = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > reachable) {
+                return false;
+            }
+            reachable = max(reachable, i + nums[i]);
+        }
+        return true;
+    }
+};
