fa5543d5ca2ee8b7a8d5747fefb69d4d443ebc0e
Drink7 <johnnyliu505@gmail.com>
2025-08-13 00:24:47 +0800

    Time: 94 ms (5.2%), Space: 70.8 MB (99.98%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray-08-13-2025-00-24-46.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // dynamic programming, 1-D
+        // TC: O(n)
+        // SC: O(n)
+        vector<int> dp(nums.size());
+        int result = nums[0];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            result = max(result, dp[i]);
+        }
+        return result;
+    }
+};
