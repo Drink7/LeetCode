70e6cb7b105f53c25b2bcd973bde0be026df8fb4
Drink7 <johnnyliu505@gmail.com>
2025-08-27 23:26:13 +0800

    Time: 0 ms (100%), Space: 11.2 MB (20.41%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs-08-27-2025-23-26-11.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        if (head->next == nullptr) {
+            return head;
+        }
+
+        ListNode* prev = swapPairs(head->next->next);
+
+        // start reverse pairs
+        int cnt = 2;
+        while (cnt > 0) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+            cnt--;
+        }
+        return prev;
+    }
+};
