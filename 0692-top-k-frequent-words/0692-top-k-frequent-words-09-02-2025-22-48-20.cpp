805c9a1a6d4b406f987cb408aac23d78de736ae9
Drink7 <johnnyliu505@gmail.com>
2025-09-02 22:48:20 +0800

    Time: 3 ms (64.93%), Space: 18.1 MB (32.84%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words-09-02-2025-22-48-20.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        // unordered_map<string, int>
+        // priority_queue<int, string>
+        // TC: O(n + m log m), m different words, n words
+        // SC: O(m)
+        unordered_map<string, int> dict;
+        vector<string> result;
+        for (auto const& s : words) {
+            dict[s]++;
+        }
+
+        auto cmp = [](const pair<int,string> &a, const pair<int,string> &b) {
+            if (a.first == b.first) {
+                return a.second < b.second;
+            } else {
+                return a.first > b.first;
+            }
+        };
+
+        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(cmp)> minHeap;
+        for (auto const& p : dict) {
+            minHeap.push({p.second, p.first});
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            result.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
