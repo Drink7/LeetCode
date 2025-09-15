3d4d73957106ff994983f95c37a27e0863ab0f32
Drink7 <johnnyliu505@gmail.com>
2025-09-16 00:14:15 +0800

    Time: 3 ms (89.36%), Space: 23.7 MB (80.09%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals-09-16-2025-00-14-14.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // sort intervals
+        // merge interval by the end
+        // TC: O(nlogn)
+        // SC: O(1)
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (result.back()[1] >= intervals[i][0]) {
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            } else {
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
