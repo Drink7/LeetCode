7f42528d3522428286f73619b6d8332957673a1c
Drink7 <johnnyliu505@gmail.com>
2025-09-23 00:35:22 +0800

    Time: 0 ms (100%), Space: 18 MB (55.03%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii-09-23-2025-00-35-20.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        // find which part is sorted
+        // TC: O(logn)
+        // SC: O(1)
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[low] == nums[mid] && nums[high] == nums[mid]) {
+                low++;
+                high--;
+            } else if (nums[mid] <= nums[high]) {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+};
