b04d43ab8382aa3db12efdd41a5740f28094d76c
Drink7 <johnnyliu505@gmail.com>
2025-09-27 21:40:59 +0800

    Time: 0 ms (100%), Space: 17.7 MB (85.5%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray-09-27-2025-21-40-58.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        // TC: O(n)
+        // SC: O(1)
+        int maxProduct = 1;
+        int minProduct = 1;
+        int result = INT_MIN;
+        for (auto const& num : nums) {
+            if (num < 0) {
+                swap(maxProduct, minProduct);
+            }
+            maxProduct = max(num, maxProduct * num);
+            minProduct = min(num, minProduct * num);
+            result = max(result, maxProduct);
+        }
+        return result;
+    }
+};
