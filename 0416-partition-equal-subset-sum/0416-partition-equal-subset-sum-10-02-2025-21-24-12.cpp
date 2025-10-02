d53adbeeaa25d0c301030eb2915c985b5b533587
Drink7 <johnnyliu505@gmail.com>
2025-10-02 21:24:12 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum-10-02-2025-21-24-12.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        // only even might be partitioned
+        // TC: O(n * sum(nums)), n is nums size
+        // SC: (n)
+        int sum = 0;
+        for (auto const& num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int n = nums.size();
+        int halfSum = sum / 2;
+        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, 0));
+        dp[0][0] = true;
+
+        // dp[i][j] means previous i numbers can sum to j?
+        // nums[i - 1] not choose, dp[i][j] = dp[i - 1][j]
+        // if j >= nums[i], nums[i - 1] choose, dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i]]
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= halfSum; j++) {
+                // nums[i - 1] not choose
+                dp[i][j] = dp[i - 1][j];
+
+                // if j >= nums[i], nums[i - 1] choose
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][halfSum];
+    }
+};
