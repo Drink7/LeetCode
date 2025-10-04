716047c9beeab8ad0d3faa277d9885cab5a07014
Drink7 <johnnyliu505@gmail.com>
2025-10-04 15:29:44 +0800

    Time: 4 ms (29.34%), Space: 8 MB (92.38%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits-10-04-2025-15-29-42.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int reversed = 0;
+        for (int i = 0; i < 32; i++) {
+            reversed = reversed << 1;
+            if (n & 1) {
+                reversed = reversed | 1;
+            }
+            n = n >> 1;
+        }
+        return reversed;
+    }
+};
