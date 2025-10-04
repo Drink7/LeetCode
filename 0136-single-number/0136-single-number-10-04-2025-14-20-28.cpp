e562f0dadc694cb01dc49cbab2df09788527ee8f
Drink7 <johnnyliu505@gmail.com>
2025-10-04 14:20:29 +0800

    Time: 0 ms (100%), Space: 20.8 MB (26.28%) - LeetHub

---
+++ 0136-single-number/0136-single-number-10-04-2025-14-20-28.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // XOR
+        // TC: O(n)
+        // SC: O(1)
+        int result = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+};
