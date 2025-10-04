4565926c46a28375596bdd12ee22ec0b6c1940ff
Drink7 <johnnyliu505@gmail.com>
2025-10-04 15:49:43 +0800

    Time: 0 ms (100%), Space: 21.8 MB (70.19%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number-10-04-2025-15-49-40.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // TC: O(n)
+        // SC: O(1)
+        int result = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+            result = result ^ i ^ nums[i];
+        }
+        return result;
+    }
+};
