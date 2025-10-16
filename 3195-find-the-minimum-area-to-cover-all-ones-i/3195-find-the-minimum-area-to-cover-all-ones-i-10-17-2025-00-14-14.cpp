7504054a8dfda246e601421396c09e99dd149f20
Drink7 <johnnyliu505@gmail.com>
2025-10-17 00:14:14 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i-10-17-2025-00-14-14.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        // find a rectangle
+        // The input is generated such that there is at least one 1 in grid.
+        // row find 1
+        // col find 1
+        // two pointers
+        // TC: O(m * n)
+        // SC: O(m + n)
+        int m = grid.size();
+        int n = grid[0].size();
+        /*
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // find height
+        int left_row = 0;
+        int right_row = m - 1;
+        while (row[left_row] != true) {
+            left_row++;
+        }
+
+        while (row[right_row] != true) {
+            right_row--;
+        }
+
+        // find width
+        int left_col = 0;
+        int right_col = n - 1;
+        while (col[left_col] != true) {
+            left_col++;
+        }
+
+        while (col[right_col] != true) {
+            right_col--;
+        }
+
+        return (right_row - left_row + 1) * (right_col - left_col + 1);
+        */
+
+        // top, down, left, right
+        int top = n, down = -1, left = m, right = -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    top = min(i, top);
+                    down = max(i, down);
+                    left = min(j, left);
+                    right = max(j, right);
+                }
+            }
+        }
+
+        if (top > down || left > right) {
+            return 0;
+        }
+
+        return (down - top + 1) * (right - left + 1);
+    }
+};
