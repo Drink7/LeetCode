15872b715f9bffeab159758a3782bb469aebb64d
Drink7 <johnnyliu505@gmail.com>
2025-10-24 00:07:05 +0800

    Time: 0 ms (100%), Space: 12.3 MB (39.49%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array-10-24-2025-00-07-04.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // i1 for nums1 pointer
+        // i2 for nums2 pointer
+        // for i = m + n - 1; i >= 0; i--
+        // when i1 >= 0 || i2 >= 0, compare nums[i1] and nums[i2], place bigger one
+        // else keep placing until both < 0
+        // TC: O(m + n)
+        // SC: O(1)
+        int i1 = m - 1;
+        int i2 = n - 1;
+        int index = m + n - 1;
+        for (; index >= 0 && i1 >= 0 && i2 >= 0; index--) {
+            if (nums1[i1] > nums2[i2]) {
+                nums1[index] = nums1[i1--];
+            } else {
+                nums1[index] = nums2[i2--];
+            }
+        }
+
+        if (i1 >= 0) {
+            for (int i = index; i >= 0; i--) {
+                nums1[i] = nums1[i1--];
+            }
+        } else {
+            for (int i = index; i >= 0; i--) {
+                nums1[i] = nums2[i2--];
+            }
+        }
+    }
+};
