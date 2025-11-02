d4eaa32d3ae8d0166d93260ee6696d60bc40ab14
Drink7 <johnnyliu505@gmail.com>
2025-11-02 14:03:10 +0800

    Time: 122 ms (46.2%), Space: 97.6 MB (89.05%) - LeetHub

---
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations-11-02-2025-14-03-09.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        // sort
+        // from the first, add the minimum (-k) and find if exist
+        // TC: O(k * nums.size()) -> TLE
+
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int prev = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            int l = max(nums[i] - k, prev + 1);
+            if (l <= nums[i] + k) {
+                prev = l;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
