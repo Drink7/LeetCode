8bff9fa44e72ac03a521b016c0f36a313a30886f
Drink7 <johnnyliu505@gmail.com>
2025-11-22 16:31:20 +0800

    Time: 0 ms (100%), Space: 9.3 MB (20.37%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv-11-22-2025-16-31-19.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        // distinct integers sum up to target
+        // dp[i] the way of possible combinations with amount i
+        // 0/1 knacksack problem
+        // TC: O(nums.size() * target)
+        // SC: O(target)
+        vector<unsigned long> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
