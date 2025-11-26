dd0dcc7ecbb9fee097f343c025ff9233d4f97830
Drink7 <johnnyliu505@gmail.com>
2025-11-26 21:28:57 +0800

    Time: 1 ms (69.6%), Space: 10.3 MB (85.9%) - LeetHub

---
+++ 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring-11-26-2025-21-28-52.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestSemiRepetitiveSubstring(string s) {
+        // longest substring -> sliding window
+        // right keep going
+        // left?
+        // at most one adjacent
+        // check right and right - 1 is right > 0, only once
+        // TC: O(n)
+        // SC: O(1)
+        int n = s.size();
+        int left = 0, right = 0;
+        int result = 0;
+        int isSame = 0;
+        while (right < n) {
+            if (right > 0 && s[right] == s[right - 1]) {
+                isSame++;
+            }
+
+            while (isSame > 1) {
+                left++;
+                if (s[left] == s[left - 1]) {
+                    isSame--;
+                }
+            }
+            result = max(result, right - left + 1);
+            right++;
+        }
+        return result;
+    }
+};
