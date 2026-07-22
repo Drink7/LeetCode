4aab12a515f04e50053b35892a59153179349495
Drink7 <johnnyliu505@gmail.com>
2026-07-22 23:49:01 +0800

    Time: 39 ms (27.56%), Space: 14.2 MB (69.97%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum-07-22-2026-23-49-00.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // TC: O(n^2)
+        // SC: O(1)
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
