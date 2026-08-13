d1f2833ad9308605ea4ad87a8d82077ca612f8d4
Drink7 <johnnyliu505@gmail.com>
2026-08-14 00:16:20 +0800

    Time: 302 ms (10.05%), Space: 81.4 MB (9.6%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters-08-14-2026-00-16-19.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // sliding window
+        // left and right, right keep moving, if the char exist in the set, check the answer and then move right
+        // TC: O(n)
+        // SC: O(n)
+        int n = s.size();
+        int left = 0, right = 0;
+        unordered_set<char> charSet;
+        int length = 0;
+        while (right < n) {
+            while (left < right && charSet.count(s[right]) > 0) {
+                charSet.erase(s[left]);
+                left++;
+            }
+
+            charSet.insert(s[right]);
+            length = max(length, right - left + 1);
+            right++;
+        }
+        return length;
+
+        // abcabcbb
+    }
+};
